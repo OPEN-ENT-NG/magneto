d54d04d9ea257b2d7329a6c7aaf89a08370bec13
Flavien Martineau <45894263+flmartineau@users.noreply.github.com>
2022-12-08 17:59:18 +0100

    chore: prepare 4.6.0 migration (#43)

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,17 @@
 var gulp = require('gulp');
 var webpack = require('webpack-stream');
 var merge = require('merge2');
-var rev = require('gulp-rev');
-var revReplace = require("gulp-rev-replace");
+const replace = require('gulp-replace');
 var clean = require('gulp-clean');
-var sourcemaps = require('gulp-sourcemaps');
 var argv = require('yargs').argv;
 var fs = require('fs');
 
 gulp.task('drop-cache', function () {
-    return gulp.src(['./src/main/resources/public/dist'], {read: false})
+    return gulp.src(['./src/main/resources/public/dist'], { read: false })
         .pipe(clean());
 });
 
-gulp.task('copy-files', ['drop-cache'], () => {
-    var html = gulp.src('./node_modules/entcore/src/template/**/*.html')
-        .pipe(gulp.dest('./src/main/resources/public/template/entcore'));
-    var bundle = gulp.src('./node_modules/entcore/bundle/*')
-        .pipe(gulp.dest('./src/main/resources/public/dist/entcore'));
-
-    return merge(html, bundle);
-})
-
-gulp.task('copy-mdi-font', ['copy-files'], () => {
+gulp.task('copy-mdi-font', ['drop-cache'], () => {
     var streams = [];
     streams
         .push(gulp.src('./node_modules/@mdi/font/fonts/*')
@@ -41,17 +30,9 @@ gulp.task('webpack', ['copy-mdi-font'], () => {
     return merge(streams);
 });
 
-gulp.task('rev', ['webpack'], () => {
-    return gulp.src('./src/main/resources/public/dist/**/*.js')
-        .pipe(rev())
-        .pipe(gulp.dest('./src/main/resources/public/dist'))
-        .pipe(rev.manifest())
-        .pipe(gulp.dest('./'));
-});
-
-gulp.task('build', ['rev'], () => {
+gulp.task('build', ['webpack'], () => {
     var refs = gulp.src("./src/main/resources/view-src/**/*.html")
-        .pipe(revReplace({manifest: gulp.src("./rev-manifest.json")}))
+        .pipe(replace('@@VERSION', Date.now()))
         .pipe(gulp.dest("./src/main/resources/view"));
 
     var copyBehaviours = gulp.src('./src/main/resources/public/dist/behaviours.js')

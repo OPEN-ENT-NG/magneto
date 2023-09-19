18da5be17e01c47e4c967c30939774c86ab7d478
rseuret <remy.seuret@cgi.com>
2023-09-19 08:01:10 +0200

    feat(Board) #MAG-234 : better css and zoomClickable

--- src/main/resources/public/ts/directives/zoom/zoom.directive.ts
+++ src/main/resources/public/ts/directives/zoom/zoom.directive.ts
@@ -1,62 +1,101 @@
 import {RootsConst} from "../../core/constants/roots.const";
-import {ng} from "entcore";
+import {ng, Me} from "entcore";
 
 
 interface IViewModel extends ng.IController, ICardProfileProps {
     zoomIn(): void;
     zoomOut(): void;
+    savePreferences(): Promise<void>
+    resetZoom():void;
+
+    zoomReset:number;
 }
 
 interface ICardProfileProps {
-    zoomLevel:number;
-    zoomMax:number;
-    zoomMin:number;
-    zoomIncrement:number;
+    zoomLevel: number;
+    zoomMax: number;
+    zoomMin: number;
+    zoomIncrement: number;
+    preferences: string;
+
 }
 
-interface IZoomScope extends ICardProfileProps , IViewModel{
+interface IZoomScope extends ICardProfileProps, IViewModel {
     vm: IViewModel;
 }
+
 class Controller implements IViewModel {
     zoomLevel: number;
-    zoomMax:number;
-    zoomMin:number;
-    zoomIncrement:number
+    zoomMax: number;
+    zoomMin: number;
+    zoomIncrement: number
+    zoomReset:number
+    preferences: string;
 
     constructor() {
-        if(this.zoomMax === undefined)
+        if (this.zoomMax === undefined)
             this.zoomMax = 150
-        if(this.zoomMin === undefined)
+        if (this.zoomMin === undefined)
             this.zoomMin = 50
-        if (this.zoomIncrement === undefined){
+        if (this.zoomIncrement === undefined) {
             this.zoomIncrement = 25
         }
     }
 
-    zoomIn(): void {
+    async $onInit() : Promise<void>{
+        await Me.preference('magneto')
+        if (!Me.preferences['magneto']) {
+            await Me.savePreference('magneto');
+            await Me.preference('magneto')
+        }
+        if (!Me.preferences['magneto'][this.preferences]) {
+            Me.preferences['magneto'][this.preferences] = this.zoomLevel;
+            await Me.savePreference('magneto');
+            await Me.preference('magneto')
+        }else{
+            this.zoomLevel = Me.preferences['magneto'][this.preferences];
+        }
+        this.zoomReset = this.zoomLevel;
+    }
+
+    async zoomIn(): Promise<void> {
         if (this.zoomLevel < this.zoomMax)
             this.zoomLevel += this.zoomIncrement;
+        await this.savePreferences();
     }
 
-    zoomOut(): void {
+    async savePreferences():Promise<void> {
+        Me.preferences['magneto'][this.preferences] = this.zoomLevel;
+        await Me.savePreference('magneto');
+    }
+
+    async zoomOut(): Promise<void> {
         if (this.zoomLevel > this.zoomMin)
             this.zoomLevel -= this.zoomIncrement;
-    }
+       Me.preferences['magneto'][this.preferences] = this.zoomLevel;
+       await Me.savePreference('magneto');
+   }
+
+   resetZoom():void {
+        this.zoomLevel = this.zoomReset;
+   }
 
 }
+
 function directive() {
     return {
         restrict: 'E',
         templateUrl: `${RootsConst.directive}zoom/zoom.html`,
         scope: {
             zoomLevel: '=',
-            zoomMax:'=?',
-            zoomMin:'=?',
-            zoomIncrement:'=?'
+            zoomMax: '=?',
+            zoomMin: '=?',
+            zoomIncrement: '=?',
+            preferences: '@'
         },
         controllerAs: 'vm',
         bindToController: true,
-        controller: ['$scope','$location','$window', Controller],
+        controller: ['$scope', '$location', '$window', Controller],
         /* interaction DOM/element */
         link: function ($scope: IZoomScope,
                         element: ng.IAugmentedJQuery,
@@ -65,4 +104,5 @@ function directive() {
         }
     }
 }
+
 export const zoom = ng.directive('zoom', directive)

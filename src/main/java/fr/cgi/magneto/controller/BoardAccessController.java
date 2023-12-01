a150c6f67c98540f1b37b1e61cb51439597df951
remy.seuret <remy.seuret@cgi.com>
2023-12-01 09:56:26 +0100

    fix(BoardAccess): #MAG-257 fix sort in mongo and java

--- src/main/java/fr/cgi/magneto/controller/BoardAccessController.java
+++ src/main/java/fr/cgi/magneto/controller/BoardAccessController.java
@@ -1,6 +1,8 @@
 package fr.cgi.magneto.controller;
 
 import fr.cgi.magneto.core.constants.Field;
+import fr.cgi.magneto.model.Section;
+import fr.cgi.magneto.model.boards.Board;
 import fr.cgi.magneto.security.ViewRight;
 import fr.cgi.magneto.service.BoardAccessService;
 import fr.cgi.magneto.service.BoardService;
@@ -9,11 +11,16 @@ import fr.wseduc.rs.ApiDoc;
 import fr.wseduc.rs.Get;
 import fr.wseduc.security.ActionType;
 import fr.wseduc.security.SecuredAction;
+import io.vertx.core.Future;
 import io.vertx.core.http.HttpServerRequest;
 import io.vertx.core.json.JsonObject;
 import org.entcore.common.controller.ControllerHelper;
 import org.entcore.common.http.filter.ResourceFilter;
 import org.entcore.common.user.UserUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class BoardAccessController extends ControllerHelper {
     BoardAccessService boardAccessService;
@@ -30,9 +37,17 @@ public class BoardAccessController extends ControllerHelper {
     @SecuredAction(value = "", type = ActionType.RESOURCE)
     public void getLastBoardsAccessed(HttpServerRequest request) {
         UserUtils.getUserInfos(eb, request, user -> {
-            boardAccessService.getLastAccess(user.getUserId())
-                    .compose(result -> boardService.getBoards(result))
-                    .onSuccess(result -> renderJson(request, new JsonObject().put(Field.ALL, result)))
+            Future<List<String>> getLastAccessFuture = boardAccessService.getLastAccess(user.getUserId());
+            getLastAccessFuture.compose(resultIds -> boardService.getBoards(resultIds))
+                    .onSuccess(boards -> {
+                        List<Board> results = new ArrayList<>();
+                        getLastAccessFuture.result().forEach(id -> boards.forEach(board -> {
+                                    if (board.getId().equals(id))
+                                        results.add(board);
+                                })
+                        );
+                        renderJson(request, new JsonObject().put(Field.ALL, results));
+                    })
                     .onFailure(fail -> {
                         String message = String.format("[Magneto@%s::getLastBoardsAccessed] Failed to get all boards by ids : %s",
                                 this.getClass().getSimpleName(), fail.getMessage());

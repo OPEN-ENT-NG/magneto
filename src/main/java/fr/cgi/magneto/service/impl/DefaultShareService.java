404831222b0185c3da3037b58224d18dc4730c90
Seuret Rémy <remy@seuret.com>
2024-01-18 16:18:43 +0100

    feat(Folder) #MAG-286 : handle share folders in back  (#102)
    * Can share folder and handle magnets rights
    
    Todo : boards share
    
    * start notifications and fix the way to keep old rights
    
    * refacto board
    
    * push public share service
    
    * wip DeleteRights
    
    * doc + remove deprecated
    
    * Handle checkRights for folder
    
    * Handle checkRights for boards
    
    * update unauthorized and more java doc
    
    * fix board without parent folder
    
    * use better function
    
    * dynamic mongoDbSharedService
    
    * pr returns + handle new folder shared
    
    * notifications except for bookmarks
    
    * indentation

---
+++ src/main/java/fr/cgi/magneto/service/impl/DefaultShareService.java
@@ -0,0 +1,215 @@
+package fr.cgi.magneto.service.impl;
+
+import fr.cgi.magneto.core.constants.Field;
+import fr.cgi.magneto.core.constants.Mongo;
+import fr.cgi.magneto.model.MongoQuery;
+import fr.cgi.magneto.model.share.SharedElem;
+import fr.cgi.magneto.service.ShareService;
+import fr.wseduc.mongodb.MongoDb;
+import io.vertx.core.Future;
+import io.vertx.core.Promise;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import org.entcore.common.mongodb.MongoDbResult;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DefaultShareService implements ShareService {
+
+    public final MongoDb mongoDb;
+    protected static final Logger log = LoggerFactory.getLogger(DefaultShareService.class);
+
+    public DefaultShareService(MongoDb mongo) {
+        this.mongoDb = mongo;
+    }
+
+    @Override
+    public Future<JsonObject> upsertSharedArray(String id, JsonObject share, String collection, boolean checkOldRights) {
+        Promise<JsonObject> promise = Promise.promise();
+        JsonArray newShare = toMongoBasicShareFormat(share);
+        JsonObject query = new JsonObject().put(Field._ID, id);
+
+        if (checkOldRights) {
+            getOldDataToUpdate(id, collection).onSuccess(success -> {
+                List<SharedElem> sharedElems = getOldRights(success.getJsonArray(Field.SHARED, new JsonArray()), newShare);
+                sharedElems.forEach(sharedElem -> {
+                    newShare.add(sharedElem.toJson());
+                });
+
+                updateMongo(collection, promise, query, newShare);
+            });
+        } else {
+            updateMongo(collection, promise, query, newShare);
+        }
+
+        return promise.future();
+    }
+
+    @Override
+    public Future<JsonObject> upsertSharedArray(String id, List<SharedElem> newShare, List<SharedElem> deletedShares, String collection, boolean checkOldRights) {
+        Promise<JsonObject> promise = Promise.promise();
+
+        JsonObject query = new JsonObject().put(Field._ID, id);
+        JsonArray shares = new JsonArray();
+
+        if (checkOldRights) {
+            getOldDataToUpdate(id, collection).onSuccess(success -> {
+                List<SharedElem> sharedElems = getOldRights(success.getJsonArray(Field.SHARED, new JsonArray()), newShare);
+                sharedElems.addAll(newShare);
+                sharedElems.forEach(sharedElem -> {
+                    if (deletedShares.stream().noneMatch(sharedElem::hasSameId)) {
+                        shares.add(sharedElem.toJson());
+                    }
+                });
+                updateMongo(collection, promise, query, shares);
+            });
+        } else {
+            newShare.forEach(elem -> shares.add(elem.toJson()));
+            updateMongo(collection, promise, query, shares);
+        }
+
+        return promise.future();
+    }
+
+    private void updateMongo(String collection, Promise<JsonObject> promise, JsonObject query, JsonArray newShare) {
+        JsonObject update = new JsonObject()
+                .put(Mongo.SET, new JsonObject()
+                        .put(Field.SHARED, newShare));
+        mongoDb.update(collection, query, update, MongoDbResult.validActionResultHandler(results -> {
+            if (results.isLeft()) {
+                String message = String.format("[Magneto@%s::updateMongo] Failed to updateMongo", this.getClass().getSimpleName());
+                log.error(String.format("%s : %s", message, results.left().getValue()));
+                promise.fail(results.left().getValue());
+            }
+            promise.complete(results.right().getValue());
+        }));
+    }
+
+
+    @Override
+    public List<SharedElem> getOldRights(JsonArray oldShared, JsonArray newShare) {
+        List<SharedElem> oldSharedElems = getSharedElemList(oldShared);
+        List<SharedElem> newSharedElems = getSharedElemList(newShare);
+
+        return getOldRights(oldSharedElems, newSharedElems);
+    }
+
+    @Override
+    public List<SharedElem> getOldRights(JsonArray oldShared, List<SharedElem> newShare) {
+        List<SharedElem> oldSharedElems = getSharedElemList(oldShared);
+
+        return getOldRights(oldSharedElems, newShare);
+    }
+
+    @Override
+    public List<SharedElem> getOldRights(List<SharedElem> oldSharedElems, List<SharedElem> newSharedElems) {
+        List<SharedElem> elementsToAdd = new ArrayList<>();
+        oldSharedElems.forEach(oldElem -> {
+            if (newSharedElems.stream().noneMatch(oldElem::hasSameId)) {
+                elementsToAdd.add(oldElem);
+            }
+        });
+        return elementsToAdd;
+    }
+
+    @Override
+    public List<SharedElem> getSharedElemList(JsonArray shares) {
+        List<SharedElem> sharedElems = new ArrayList<>();
+        shares.forEach(share -> {
+            JsonObject newShareElemJO = (JsonObject) share;
+            SharedElem sharedElem = new SharedElem();
+            sharedElem.set(newShareElemJO);
+            sharedElems.add(sharedElem);
+        });
+        return sharedElems;
+    }
+
+    @Override
+    public List<SharedElem> getSharedElemList(JsonObject shares) {
+        return getSharedElemList(toMongoBasicShareFormat(shares));
+    }
+
+    private static JsonArray toMongoBasicShareFormat(JsonObject shareJson) {
+        JsonArray resultJsonArray = new JsonArray();
+        processShareJsonField(shareJson, Field.USERS, Field.USERID, resultJsonArray);
+        processShareJsonField(shareJson, Field.GROUPS, Field.GROUPID, resultJsonArray);
+        processShareJsonField(shareJson, Field.BOOKMARKS, Field.BOOKMARKID, resultJsonArray);
+        return resultJsonArray;
+
+    }
+
+    private static void processShareJsonField(JsonObject shareJson, String fieldKey, String idKey, JsonArray resultJsonArray) {
+        if (shareJson.containsKey(fieldKey)) {
+            shareJson.getJsonObject(fieldKey, new JsonObject()).forEach(entry -> {
+                String id = entry.getKey();
+                JsonArray rights = (JsonArray) entry.getValue();
+                JsonObject object = new JsonObject().put(idKey, id);
+                processRights(rights, object);
+                resultJsonArray.add(object);
+            });
+        }
+    }
+
+    private static void processRights(JsonArray rights, JsonObject targetObject) {
+        rights.forEach(right -> targetObject.put(right.toString(), true));
+    }
+
+    @Override
+    public Future<JsonObject> getOldDataToUpdate(String folderId, String collection) {
+        Promise<JsonObject> promise = Promise.promise();
+        MongoQuery query = new MongoQuery(collection);
+        query.match(new JsonObject().put(Field._ID, folderId));
+        mongoDb.command(query.getAggregate().toString(), MongoDbResult.validResultHandler(resultMongo -> {
+            if (resultMongo.isRight()) {
+                JsonObject result = resultMongo.right().getValue()
+                        .getJsonObject(Mongo.CURSOR, new JsonObject())
+                        .getJsonArray(Mongo.FIRSTBATCH, new JsonArray())
+                        .getJsonObject(0);
+                promise.complete(result);
+            } else {
+                String message = String.format("[Magneto@%s::getOldDataToUpdate] Failed to update old folder",
+                        this.getClass().getSimpleName());
+                log.error(String.format("%s : %s", message, resultMongo.left().getValue()));
+                promise.fail(message);
+            }
+        }));
+        return promise.future();
+    }
+
+    @Override
+    public Future<List<SharedElem>> getDeletedRights(String id, List<SharedElem> newSharedElem, String boardCollection) {
+        Promise<List<SharedElem>> promise = Promise.promise();
+        getOldDataToUpdate(id, boardCollection).onSuccess(success -> {
+            promise.complete(getOldRights(success.getJsonArray(Field.SHARED, new JsonArray()), newSharedElem));
+        });
+        return promise.future();
+    }
+
+    @Override
+    public Future<Boolean> checkParentRights(String id, List<SharedElem> newSharedElem, String collection) {
+        Promise<Boolean> promise = Promise.promise();
+        this.getOldDataToUpdate(id, collection).onSuccess(folder -> {
+            if (folder.getValue(Field.PARENTID) == null) {
+                promise.complete(true);
+            } else {
+                this.getOldDataToUpdate(folder.getString(Field.PARENTID, ""), collection)
+                        .onSuccess(parent -> {
+                            promise.complete(checkRights(newSharedElem, parent));
+                        }).onFailure(error -> promise.fail(error.getMessage()));
+            }
+        }).onFailure(error -> promise.fail(error.getMessage()));
+
+        return promise.future();
+    }
+
+    @Override
+    public boolean checkRights(List<SharedElem> newSharedElem, JsonObject parent) {
+        List<SharedElem> parentRights = getSharedElemList(parent.getJsonArray(Field.SHARED, new JsonArray()));
+
+        return parentRights.stream().allMatch(parentRight ->
+                newSharedElem.stream().noneMatch(elem -> parentRight.hasSameId(elem) && parentRight.getRights().size() > elem.getRights().size()));
+    }
+}

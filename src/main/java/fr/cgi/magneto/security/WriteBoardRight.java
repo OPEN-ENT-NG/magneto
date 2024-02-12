f9cae5a2b9339d4ed35820cd70769202c2f1575e
alicedraillard <85180705+alicedraillard@users.noreply.github.com>
2024-02-12 16:44:47 +0100

    fix(rights): #MAG-347 fix action rights (#142)

--- src/main/java/fr/cgi/magneto/security/WriteBoardRight.java
+++ src/main/java/fr/cgi/magneto/security/WriteBoardRight.java
@@ -17,6 +17,14 @@ public class WriteBoardRight implements ResourcesProvider {
 
         RequestUtils.bodyToJson(request, body -> {
             String boardId = body.getString(Field.BOARDID, request.getParam(Field.BOARDID));
+            JsonObject sharedUserCondition = new JsonObject()
+                    .put(Field.USERID, user.getUserId())
+                    .put(Rights.SHAREBOARDCONTROLLER_INITPUBLISHRIGHT, true);
+
+            JsonObject sharedGroupCondition = new JsonObject()
+                    .put(Field.GROUPID, new JsonObject().put(Mongo.IN, user.getGroupsIds()))
+                    .put(Rights.SHAREBOARDCONTROLLER_INITPUBLISHRIGHT, true);
+
             JsonObject query = new JsonObject()
                     .put(Field._ID, boardId)
                     .put(Field.DELETED, false)
@@ -24,14 +32,9 @@ public class WriteBoardRight implements ResourcesProvider {
                             new JsonArray()
                                     .add(new JsonObject()
                                             .put(Field.OWNERID, user.getUserId()))
-                                    .add(new JsonObject()
-                                            .put(String.format("%s.%s", Field.SHARED, Field.USERID),
-                                                    new JsonObject().put(Mongo.IN, new JsonArray().add(user.getUserId())))
-                                            .put(String.format("%s.%s", Field.SHARED, "fr-cgi-magneto-controller-ShareBoardController|initPublishRight"), true))
-                                    .add(new JsonObject()
-                                            .put(String.format("%s.%s", Field.SHARED, Field.GROUPID),
-                                                    new JsonObject().put(Mongo.IN, user.getGroupsIds()))
-                                            .put(String.format("%s.%s", Field.SHARED, "fr-cgi-magneto-controller-ShareBoardController|initPublishRight"), true)));
+                                    .add(new JsonObject().put(Field.SHARED, new JsonObject().put(Mongo.ELEMMATCH, sharedUserCondition)))
+                                    .add(new JsonObject().put(Field.SHARED, new JsonObject().put(Mongo.ELEMMATCH, sharedGroupCondition)))
+                    );
             MongoAppFilter.executeCountQuery(request, CollectionsConstant.BOARD_COLLECTION, query, 1, res -> {
                 handler.handle(Boolean.TRUE.equals(res) && WorkflowHelper.hasRight(user, Rights.MANAGE_BOARD));
             });

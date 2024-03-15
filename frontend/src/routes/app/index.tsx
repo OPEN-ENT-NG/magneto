aa5c3c8681852a6b52c38e32f96e43b814b58e60
alice.draillard <alice.draillard@cgi.com>
2024-03-15 14:55:51 +0100

    feat(react): #MAG-389 add base blocks for module view

--- frontend/src/routes/app/index.tsx
+++ frontend/src/routes/app/index.tsx
@@ -4,6 +4,7 @@ import { ID } from "edifice-ts-client";
 import { Link } from "react-router-dom";
 
 import { Card } from "~/components/card/Card.tsx";
+import { Heading, TreeView } from "@edifice-ui/react";
 
 // const ExportModal = lazy(async () => await import("~/features/export-modal"));
 
@@ -23,14 +24,102 @@ export const App = () => {
   console.log("i am in app");
   return (
     <>
-      <div>root index principal</div>
-      <Card title="Sample Card" content="This is a sample card component." />
-      <Link to={`/user`}>click to access user </Link>
-      <Link to={`/info`}>click to access info </Link>
-      <Link to={`/board/{id}/view`}>click to access board </Link>
-      <Link to={`/board/{id}/reading`}>click to access read </Link>
-      <Link to={`/`}>click to access /</Link>
-      <p>hello</p>
+      <Heading headingStyle="h1"level="h1">
+        Magneto
+      </Heading>
+    <>
+      <TreeView
+        data={{
+          children: [
+            {
+              children: [
+                {
+                  children: [
+                    {
+                      children: [
+                        {
+                          id: '12',
+                          name: 'level 4 arborescence tree'
+                        },
+                        {
+                          id: '13',
+                          name: 'level 4 arborescence tree'
+                        }
+                      ],
+                      id: '8',
+                      name: 'level 3 arborescence tree'
+                    },
+                    {
+                      id: '9',
+                      name: 'level 3 arborescence tree'
+                    }
+                  ],
+                  id: '4',
+                  name: 'level 2 arborescence tree'
+                },
+                {
+                  children: [
+                    {
+                      id: '10',
+                      name: 'level 3 arborescence tree'
+                    },
+                    {
+                      id: '11',
+                      name: 'level 3 arborescence tree'
+                    }
+                  ],
+                  id: '5',
+                  name: 'level 2 arborescence tree'
+                }
+              ],
+              id: '1',
+              name: 'level 1 arborescence tree'
+            },
+            {
+              children: [
+                {
+                  id: '6',
+                  name: 'level 2 arborescence tree'
+                },
+                {
+                  id: '7',
+                  name: 'level 2 arborescence tree'
+                }
+              ],
+              id: '2',
+              name: 'level 1 arborescence tree'
+            },
+            {
+              id: '3',
+              name: 'level 1 arborescence tree'
+            }
+          ],
+          id: 'root',
+          name: 'Section Element',
+          section: true
+        }}
+        onTreeItemBlur={function Ga(){}}
+        onTreeItemFocus={function Ga(){}}
+        onTreeItemFold={function Ga(){}}
+        onTreeItemSelect={function Ga(){}}
+        onTreeItemUnfold={function Ga(){}}
+      />
+      <div
+        className="bg-light position-absolute p-8"
+        style={{
+          fontSize: '1rem',
+          right: '8px',
+          top: '8px'
+        }}
+      >
+        <span>
+          Events history (for debug only/cleared after 20 events):
+        </span>
+        <ul />
+      </div>
+    </>
+      
+      
     </>
   );
 };

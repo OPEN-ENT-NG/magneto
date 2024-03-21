42a2043b208e4550e1e4a4293250fc17caba9432
FlorentMr <florent.mariotti@cgi.com>
2024-03-21 16:13:35 +0100

    prettier in

--- frontend/src/config/index.ts
+++ frontend/src/config/index.ts
@@ -1,22 +1,22 @@
 export const workflows = {
-  view: 'fr.cgi.magneto.controller.MagnetoController|view',
-  manage: 'fr.cgi.magneto.controller.FakeRight|boardManage',
-  publish: 'fr.cgi.magneto.controller.FakeRight|boardPublish',
-  comment: 'fr.cgi.magneto.controller.FakeRight|boardComment',
-  favorites: 'fr.cgi.magneto.controller.FakeRight|boardFavorites',
+  view: "fr.cgi.magneto.controller.MagnetoController|view",
+  manage: "fr.cgi.magneto.controller.FakeRight|boardManage",
+  publish: "fr.cgi.magneto.controller.FakeRight|boardPublish",
+  comment: "fr.cgi.magneto.controller.FakeRight|boardComment",
+  favorites: "fr.cgi.magneto.controller.FakeRight|boardFavorites",
 };
 
 export const rights = {
   read: {
-      right: "fr-cgi-magneto-controller-ShareBoardController|initReadRight"
+    right: "fr-cgi-magneto-controller-ShareBoardController|initReadRight",
   },
   publish: {
-      right: "fr-cgi-magneto-controller-ShareBoardController|initPublishRight"
+    right: "fr-cgi-magneto-controller-ShareBoardController|initPublishRight",
   },
   contrib: {
-      right: "fr-cgi-magneto-controller-ShareBoardController|initContribRight"
+    right: "fr-cgi-magneto-controller-ShareBoardController|initContribRight",
   },
   manager: {
-      right: "fr-cgi-magneto-controller-ShareBoardController|initManagerRight"
-  }
+    right: "fr-cgi-magneto-controller-ShareBoardController|initManagerRight",
+  },
 };

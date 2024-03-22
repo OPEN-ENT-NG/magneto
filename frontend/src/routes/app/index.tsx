badaccec72a325e155a6f3fbe45d6c9a73bd9d65
Florent Mariotti <florentcorse@gmail.com>
2024-03-22 10:37:08 +0100

    feat(routes) : #MAG-388 add all main routes of magneto (#173)
    * feat(routes) : #MAG-388 add all main routes of magneto
    
    * fix prettier
    
    ---------
    
    Co-authored-by: FlorentMr <florent.mariotti@cgi.com>

--- frontend/src/routes/app/index.tsx
+++ frontend/src/routes/app/index.tsx
@@ -27,6 +27,8 @@ export const App = () => {
       <Card title="Sample Card" content="This is a sample card component." />
       <Link to={`/user`}>click to access user </Link>
       <Link to={`/info`}>click to access info </Link>
+      <Link to={`/board/{id}/view`}>click to access board </Link>
+      <Link to={`/board/{id}/reading`}>click to access read </Link>
       <Link to={`/`}>click to access /</Link>
       <p>hello</p>
     </>

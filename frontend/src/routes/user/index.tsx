d122c158e813b88dd30f23400cbbc5ff6c3e392c
florent.mariotti <florent.mariotti@cgi.com>
2024-03-22 10:37:08 +0100

    feat(routes) : #MAG-388 add all main routes of magneto (#173)
    * feat(routes) : #MAG-388 add all main routes of magneto
    
    * fix prettier
    
    ---------
    
    Co-authored-by: FlorentMr <florent.mariotti@cgi.com>

--- frontend/src/routes/user/index.tsx
+++ frontend/src/routes/user/index.tsx
@@ -24,6 +24,8 @@ export const App = () => {
       <div>coucou je suis user</div>
       <Link to={`/user`}>click to access user </Link>
       <Link to={`/info`}>click to access info </Link>
+      <Link to={`/board/{id}/view`}>click to access board </Link>
+      <Link to={`/board/{id}/reading`}>click to access read </Link>
       <Link to={`/`}>click to access /</Link>
     </>
   );

db45f20c1f4cb8a5ce4da83be81ca16d9b1dbf46
florent.mariotti <florent.mariotti@cgi.com>
2024-03-25 11:57:09 +0100

    feat(react): #MAG-389 add base blocks for module view (#174)
    * feat(react): #MAG-389 add base blocks for module view
    
    * adjust order and added usage of Card component
    
    ---------
    
    Co-authored-by: alice.draillard <alice.draillard@cgi.com>
    Co-authored-by: FlorentMr <florent.mariotti@cgi.com>

--- frontend/src/routes/app/index.tsx
+++ frontend/src/routes/app/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
+import { Heading, TreeView, Grid } from "@edifice-ui/react";
 import { ID } from "edifice-ts-client";
-import { Link } from "react-router-dom";
 
 import { Card } from "~/components/card/Card.tsx";
 
@@ -23,12 +23,106 @@ export const App = () => {
   console.log("i am in app");
   return (
     <>
-      <div>root index principal</div>
-      <Card title="Sample Card" content="This is a sample card component." />
-      <Link to={`/user`}>click to access user </Link>
-      <Link to={`/info`}>click to access info </Link>
-      <Link to={`/`}>click to access /</Link>
-      <p>hello</p>
+      <Heading headingStyle="h1" level="h1">
+        Magneto
+      </Heading>
+      <>
+        <Grid>
+          <Grid.Col
+            sm="4"
+            style={{
+              minHeight: "70rem",
+              padding: ".8rem",
+            }}
+          >
+            <TreeView
+              data={{
+                children: [
+                  {
+                    children: [
+                      {
+                        children: [
+                          {
+                            children: [
+                              {
+                                id: "12",
+                                name: "level 4 arborescence tree",
+                              },
+                              {
+                                id: "13",
+                                name: "level 4 arborescence tree",
+                              },
+                            ],
+                            id: "8",
+                            name: "level 3 arborescence tree",
+                          },
+                          {
+                            id: "9",
+                            name: "level 3 arborescence tree",
+                          },
+                        ],
+                        id: "4",
+                        name: "level 2 arborescence tree",
+                      },
+                      {
+                        children: [
+                          {
+                            id: "10",
+                            name: "level 3 arborescence tree",
+                          },
+                          {
+                            id: "11",
+                            name: "level 3 arborescence tree",
+                          },
+                        ],
+                        id: "5",
+                        name: "level 2 arborescence tree",
+                      },
+                    ],
+                    id: "1",
+                    name: "level 1 arborescence tree",
+                  },
+                  {
+                    children: [
+                      {
+                        id: "6",
+                        name: "level 2 arborescence tree",
+                      },
+                      {
+                        id: "7",
+                        name: "level 2 arborescence tree",
+                      },
+                    ],
+                    id: "2",
+                    name: "level 1 arborescence tree",
+                  },
+                  {
+                    id: "3",
+                    name: "level 1 arborescence tree",
+                  },
+                ],
+                id: "root",
+                name: "Section Element",
+                section: true,
+              }}
+              onTreeItemBlur={function Ga() {}}
+              onTreeItemFocus={function Ga() {}}
+              onTreeItemFold={function Ga() {}}
+              onTreeItemSelect={function Ga() {}}
+              onTreeItemUnfold={function Ga() {}}
+            />
+          </Grid.Col>
+          <Grid.Col
+            sm="8"
+            style={{
+              minHeight: "10rem",
+              padding: ".8rem",
+            }}
+          >
+            <Card title={"Main"} content={"NON"} />
+          </Grid.Col>
+        </Grid>
+      </>
     </>
   );
 };

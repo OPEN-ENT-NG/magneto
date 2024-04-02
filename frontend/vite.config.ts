3f2c0e3bddb75682dfe45edead7b41c1b59c8167
FlorentMr <florent.mariotti@cgi.com>
2024-04-02 16:34:50 +0200

    PR returns

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,6 @@
 
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from 'path'
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/

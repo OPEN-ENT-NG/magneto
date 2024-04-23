71b2bbae9a8c5afd669afe8ca5e69d70c1a07586
alice.draillard <alice.draillard@cgi.com>
2024-04-23 17:56:25 +0200

    feat(react): #MAG-391 display main page

--- frontend/src/routes/app/index.tsx
+++ frontend/src/routes/app/index.tsx
@@ -1,4 +1,7 @@
-import { Grid, useToggle } from "@edifice-ui/react";
+import React from "react";
+import { useState } from 'react';
+
+import { Heading, Grid, useToggle } from "@edifice-ui/react";
 import { ID } from "edifice-ts-client";
 import { useTranslation } from "react-i18next";
 
@@ -6,11 +9,16 @@ import { Card } from "~/components/card/Card.tsx";
 import { CreateTab } from "~/components/create-tab/createTab";
 import Header from "~/components/header/Header";
 import { TreeViewButtons } from "~/components/tree-view/TreeViewButtons";
-import { TreeViewContainer } from "~/components/tree-view/TreeViewContainer";
+import { useGetBoardsQuery } from "~/services/api/boards.service";
+import { useGetFoldersQuery } from "~/services/api/folders.service";
+import { formControlClasses } from "@mui/material";
+import { FolderTreeNavItem, IFolderTreeNavItem } from "~/models/folder-tree.model";
 import { FOLDER_TYPE } from "~/core/enums/folder-type.enum";
 import { FolderTreeNavItem } from "~/models/folder-tree.model";
 import { Folder, IFolderResponse } from "~/models/folder.model";
 import { useGetFoldersQuery } from "~/services/api/folders.service";
+import { Board, IBoardItemResponse } from "~/models/board.model";
+import { SideBar } from "~/components/side-bar/SideBar";
 
 export interface AppProps {
   _id: string;
@@ -139,6 +147,32 @@ export const App = () => {
   } = useGetFoldersQuery(true);
   console.log(myFoldersResult);
 
+  const [boards, setBoards] = useState([]);
+  const [folders, setFolders] = useState([]);
+  const [currentFolderChildren, setCurrentFolderChildren] = useState([]);
+  const [selectedBoardIds, setSelectedBoardIds] = useState([]);
+  const [selectedBoards, setSelectedBoards] = useState([]);
+  const [selectedFolderIds, setSelectedFolderIds] = useState([]);
+  const [selectedFolders, setSelectedFolders] = useState([]);
+  const [folderNavTrees, setFolderNavTrees] = useState<FolderTreeNavItem[]>([new FolderTreeNavItem({
+      id: FOLDER_TYPE.MY_BOARDS, title: t('magneto.my.boards'), 
+      parentId: '', section: true
+  }, false, "magneto-check-decagram")]);
+  const [publicFolderNavTrees, setpublicFolderNavTrees] = useState<FolderTreeNavItem[]>([new FolderTreeNavItem({
+    id: FOLDER_TYPE.PUBLIC_BOARDS, title: t('magneto.lycee.connecte.boards'), 
+    parentId: '', section: true
+}, false, "magneto-earth")]);
+  const [deletedFolderNavTrees, setdeletedFolderNavTrees] = useState<FolderTreeNavItem[]>([new FolderTreeNavItem({
+    id: FOLDER_TYPE.DELETED_BOARDS, title: t('magneto.trash'),
+    parentId: '', section: true
+}, false, "magneto.trash")]);
+  const [folderMoveNavTrees, setFolderMoveNavTrees] = useState([]);
+
+
+  
+  const { data: myFoldersResult, isLoading: getFoldersLoading, error: getFoldersError } = useGetFoldersQuery(false);
+  const { data: deletedFoldersResult, isLoading: getDeletedFoldersLoading, error: getDeletedFoldersError } = useGetFoldersQuery(true);
+
   let myFoldersObject;
   let deletedFoldersObject;
 
@@ -157,6 +191,24 @@ export const App = () => {
     myFoldersObject = folderNavTrees[0].buildFolders(myFolders);
     deletedFoldersObject =
       deletedFolderNavTrees[0].buildFolders(deletedFolders);
+
+    myFoldersObject = folderNavTrees[0].buildFolders(myFolders);
+    deletedFoldersObject = deletedFolderNavTrees[0].buildFolders(deletedFolders);
+  }
+
+  const { data: myBoardsResult, isLoading: getBoardsLoading, error: getBoardsError } = useGetBoardsQuery({
+    isPublic: false, isShared: true, isDeleted: false, sortBy: 'modificationDate'});
+  console.log(myBoardsResult);
+
+ 
+  if (getBoardsError) {
+    console.log("Boards error");
+  } else if (getBoardsLoading) {
+    console.log("Boards loading");
+  } else { 
+    setBoards(myBoardsResult.map(((board: IBoardItemResponse) => new Board().build(board)))); //convert to Board[]
+    console.log(boards);
+
   }
 
   return (
@@ -171,6 +223,7 @@ export const App = () => {
             padding: ".8rem",
           }}
         >
+
           <aside className="g-col-3 g-col-lg-2 g-col-xl-3 border-end pt-16 pe-16 d-none d-lg-block">
             <TreeViewContainer
               folders={myFoldersObject}
@@ -191,6 +244,11 @@ export const App = () => {
             />
             <TreeViewButtons />
           </aside>
+=======
+
+        <SideBar />
+
+>>>>>>> 8076f6a... feat(react): #MAG-391 display main page
         </Grid.Col>
         <Grid.Col
           sm="8"

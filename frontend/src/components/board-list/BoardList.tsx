27a1fd6474d058264b407f55e978efe6f168daf0
alice.draillard <alice.draillard@cgi.com>
2024-05-17 19:41:48 +0200

    424

--- frontend/src/components/board-list/BoardList.tsx
+++ frontend/src/components/board-list/BoardList.tsx
@@ -1,103 +1,108 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import dayjs from 'dayjs';
 
-import { Card, useOdeClient, Tooltip } from "@edifice-ui/react";
-import {
-  mdiAccountCircle,
-  mdiCalendarBlank,
-  mdiCrown,
-  mdiEarth,
-  mdiMagnet,
-  mdiShareVariant,
-} from "@mdi/js";
-import { Icon } from "@mdi/react";
-import { animated, useSpring } from "@react-spring/web";
-import dayjs from "dayjs";
-import { useTranslation } from "react-i18next";
+import { Card, useOdeClient, ActionBar, Tooltip } from "@edifice-ui/react";
+import {  animated, useSpring } from "@react-spring/web";
+
+import { useToaster } from "../../hooks/useToaster"
 
 import "./BoardList.scss";
 import { Board, IBoardItemResponse } from "~/models/board.model";
 import { useGetBoardsQuery } from "~/services/api/boards.service";
+import { Board, IBoardItemResponse, IBoardsParamsRequest, IBoardsResponse } from "~/models/board.model";
+import { mdiAccountCircle, mdiCalendarBlank, mdiCrown, mdiEarth, mdiMagnet, mdiShareVariant } from "@mdi/js";
+import { Icon } from "@mdi/react";
+import { useTranslation } from "react-i18next";
+
+
+
 
 export const BoardList = () => {
-  const { user, currentApp } = useOdeClient();
-  const { t } = useTranslation();
-  // const [isToasterOpen, setIsToasterOpen] = useToaster();
+    const { user, currentApp } = useOdeClient();
+    const { t } = useTranslation();
+    const [currentFolder, setCurrentFolder] = useState(new Folder());
 
   const userId = user ? user?.userId : "";
 
-  const {
-    data: myBoardsResult,
-    isLoading: getBoardsLoading,
-    error: getBoardsError,
-  } = useGetBoardsQuery({
-    isPublic: false,
-    isShared: true,
-    isDeleted: false,
-    sortBy: "modificationDate",
-    page: 0,
-  }) || {};
-
-  let boardData: Board[];
-  if (getBoardsError) {
-    console.log("error");
-  } else if (getBoardsLoading) {
-    console.log("loading");
-  } else {
-    boardData = myBoardsResult.all.map((board: IBoardItemResponse) =>
-      new Board().build(board),
-    ); //convert boards to Board[]
-    console.log("boardData", boardData);
-  }
+    let boardData;
+    let boardsQuery: IBoardsParamsRequest = {
+        isPublic: false,
+        isShared: true,
+        isDeleted: false,
+        sortBy: 'modificationDate',
+        page: 0
+    }
+    
+    const getFolderBoards = (): void => {
+        if (!currentFolder.id || currentFolder.id == "my-boards" || currentFolder.id == "") {
+            boardsQuery.folderId = undefined;
+        } else if (!!currentFolder && !!currentFolder.id) { 
+            boardsQuery.folderId = currentFolder.id;
+        } else {
+            console.log("currentFolder undefined, try later or again");
+        }
+    }
 
   const springs = useSpring({
     from: { opacity: 0 },
     to: { opacity: 1 },
   });
 
-  const isSameAsUser = (id: string) => {
-    return id == userId;
-  };
+    const isSameAsUser = (id: string) => {
+        return id == userId;
+    }
+
+    useEffect(() => {
+        getFolderBoards();
+    }, [currentFolder]);
+
+    const { data: myBoardsResult, isLoading: getBoardsLoading, error: getBoardsError } = useGetBoardsQuery(boardsQuery) || {};
+    
+    if (getBoardsError) {
+        console.log("error");
+    } else if (getBoardsLoading) {
+        console.log("loading");
+    } else {
+        boardData = myBoardsResult.all.map(((board: IBoardItemResponse) => new Board().build(board))); //convert boards to Board[]
+        console.log("boardData", boardData);
+    }
+
 
   return (
     <>
       {boardData?.length ? (
         <animated.ul className="grid ps-0 list-unstyled mb-24">
-          {boardData.map((board: Board) => {
-            const {
-              id,
-              title,
-              imageUrl,
-              nbCards,
-              shared,
-              owner,
-              modificationDate,
-              isPublished,
-            } = board;
-            return (
-              <animated.li
-                className="g-col-4 z-1 boardSizing"
-                key={id}
-                style={{
-                  position: "relative",
-                  ...springs,
-                }}
-              >
-                <Card
-                  app={currentApp!}
-                  options={{
-                    type: "board",
-                    title,
-                  }}
-                  // onClick={() => {setIsToasterOpen()}}
-                  isLoading={getBoardsLoading}
-                  isSelectable={false}
-                >
-                  <Card.Body flexDirection={"column"}>
-                    <Card.Image
-                      imageSrc={imageUrl}
-                      variant="landscape"
-                    ></Card.Image>
-                    <Card.Title>{title}</Card.Title>
+            {boardData.map((board: Board) => {
+                const { id, title, imageUrl, nbCards, shared, owner, modificationDate, isPublished } = board;
+                return(
+                    <animated.li
+                        className="g-col-4 z-1 boardSizing"
+                        key={id}
+                        style={{
+                        position: "relative",
+                        ...springs
+                        }}
+                    >
+                        <Card
+                            app={currentApp!}
+                            options={{
+                                type: "board",
+                                title,
+                            }}
+                            // onClick={() => {setIsToasterOpen()}}
+                            isLoading={getBoardsLoading}
+                        >
+                            <Card.Body 
+                                flexDirection={"column"}>
+
+                                <Card.Image
+                                    imageSrc={imageUrl}
+                                    variant="landscape">
+
+                                </Card.Image>
+                                <Card.Title>
+                                    {title}
+                                </Card.Title>
 
                     <div className="board-number-magnets">
                       <Icon
--- frontend/src/components/folder-list/FolderList.tsx
+++ frontend/src/components/folder-list/FolderList.tsx
@@ -10,36 +10,51 @@ import { Folder, IFolderResponse } from "~/models/folder.model";
 import { useGetFoldersQuery } from "~/services/api/folders.service";
 
 export const FolderList = () => {
-  const { currentApp } = useOdeClient();
-  // const [isToasterOpen, setIsToasterOpen] = useToaster();
+    const { currentApp } = useOdeClient();
+    // const [isToasterOpen, setIsToasterOpen] = useToaster();
+    const [currentFolder, setCurrentFolder] = useState(new Folder());
 
-  const {
-    data: myFoldersResult,
-    isLoading: getFoldersLoading,
-    error: getFoldersError,
-  } = useGetFoldersQuery(false);
+    const { data: myFoldersResult, isLoading: getFoldersLoading, error: getFoldersError } = useGetFoldersQuery(false);
+    const { data: deletedFoldersResult, isLoading: getDeletedFoldersLoading, error: getDeletedFoldersError } = useGetFoldersQuery(true);
 
-  let folderData;
-  if (getFoldersError) {
-    console.log("error");
-  } else if (getFoldersLoading) {
-    console.log("loading");
-  } else {
-    console.log("myFoldersResult", myFoldersResult);
-    folderData = myFoldersResult.map((folder: IFolderResponse) =>
-      new Folder().build(folder),
-    ); //convert folders to Folder[]
-  }
+    const filterFolderData = (): void => {
+        if (!currentFolder.id || currentFolder.id == "my-boards" || currentFolder.id == "") {
+            folderData = folderData.filter((folder: Folder) => !folder.parentId);
+        } else if (!!currentFolder && !!currentFolder.id) { 
+            folderData = folderData.filter((folder: Folder) => folder.parentId == currentFolder.id);
+        } else {
+            console.log("currentFolder undefined, try later or again");
+        }
+    }
+
+    let folderData: Folder[];
+    let deletedFolders;
+    if (getFoldersError || getDeletedFoldersError) {
+        console.log("error");
+    } else if (getFoldersLoading || getDeletedFoldersLoading) {
+        console.log("loading");
+    } else {
+        console.log("myFoldersResult", myFoldersResult);
+        folderData = myFoldersResult.map(((folder: IFolderResponse) => new Folder().build(folder))); //convert folders to Folder[]
+        filterFolderData();
+        console.log("folderData", folderData);
+        deletedFolders = deletedFoldersResult.map(((folder: IFolderResponse) => new Folder().build(folder)));
+    }
 
   const springs = useSpring({
     from: { opacity: 0 },
     to: { opacity: 1 },
   });
 
+    useEffect(() => {
+        if (myFoldersResult) filterFolderData();
+    }, [currentFolder]);
+
   return (
     <>
       {folderData?.length ? (
         <animated.ul className="grid ps-0 list-unstyled mb-24">
+<<<<<<< HEAD
           {folderData.map((folder: Folder) => {
             const { id, title } = folder;
             return (
@@ -75,6 +90,42 @@ export const FolderList = () => {
           })}
         </animated.ul>
       ) : null}
+=======
+            {folderData.map((folder: Folder) => {
+                const { id, title } = folder;
+                return(
+                    <animated.li
+                        className="g-col-4 z-1 folderSizing"
+                        key={id}
+                        style={{
+                        position: "relative",
+                        ...springs
+                        }}
+                    >
+                        <Card
+                            app={currentApp!}
+                            options={{
+                                type: "folder",
+                                title,
+                            }}
+                            // onClick={() => {setIsToasterOpen()}}
+                            isLoading={getFoldersLoading || getDeletedFoldersLoading}
+                            onClick={() => {setCurrentFolder(folder)}}
+                        >
+                            <Card.Body>
+                                <Icon path={mdiFolderPlus} size={1}></Icon>
+                                <Card.Title>
+                                {title}
+                                </Card.Title>
+                            </Card.Body>
+                        </Card>
+                    </animated.li>
+                    
+                )
+            })}
+        </animated.ul> 
+        ) : null}
+>>>>>>> 424
     </>
   );
 };

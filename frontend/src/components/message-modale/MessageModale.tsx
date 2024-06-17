fb02bd7526f9c4e1c692af330920cd359014bdb2
alice.draillard <alice.draillard@cgi.com>
2024-06-17 18:12:39 +0200

    modal start

---
+++ frontend/src/components/message-modale/MessageModale.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+
+import { Button, Modal } from "@edifice-ui/react";
+
+import "./FolderItem.scss";
+import { title } from "process";
+import { useTranslation } from "react-i18next";
+
+type MessageModaleProps = {
+    isOpen: boolean;
+    key: string;
+    param: string;
+    hasSubmit: boolean;
+    onSubmit:() => void;
+    onCancel: () => void;
+};
+
+export const MessageModale: React.FunctionComponent<MessageModaleProps> = ({
+    isOpen,
+    key,
+    param,
+    hasSubmit,
+    onSubmit,
+    onCancel
+}) => {
+    const { t } = useTranslation();
+    
+    return (
+        <>
+            {isOpen && (
+                <Modal
+                id={"displayMessage"}
+                isOpen={isOpen}
+                onModalClose={onCancel}
+                size="md"
+                viewport={false}
+                >
+                <Modal.Body>
+                    {t(key, param)}
+                </Modal.Body>
+
+                <Modal.Footer>
+                    <div className="right">
+                    <Button
+                        color="primary"
+                        type="button"
+                        variant="outline"
+                        className="footer-button"
+                        onClick={onCancel}
+                    >
+                        {t("magneto.cancel")}
+                    </Button>
+                    {hasSubmit && <Button
+                        color="primary"
+                        type="submit"
+                        variant="filled"
+                        className="footer-button"
+                        disabled={title == ""}
+                        onClick={onSubmit}
+                    >
+                    { t("magneto.confirm")}
+                    </Button>}
+                    </div>
+                </Modal.Footer>
+                </Modal>
+            )}
+                
+        </>
+  );
+};

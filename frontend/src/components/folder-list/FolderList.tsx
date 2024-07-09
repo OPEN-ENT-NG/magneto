ecefd53013eee5b3c5df631b31bc1f2824f92dae
alicedraillard <85180705+alicedraillard@users.noreply.github.com>
2024-07-09 17:56:29 +0200

    feat(dragAndDrop): #MAG-392 implement drag and drop (#190)

--- frontend/src/components/folder-list/FolderList.tsx
+++ frontend/src/components/folder-list/FolderList.tsx
@@ -1,35 +1,39 @@
 import React, { useEffect, useState } from "react";
 
-import { Card, useOdeClient } from "@edifice-ui/react";
-import { mdiFolderPlus } from "@mdi/js";
-import { Icon } from "@mui/material";
 import { animated, useSpring } from "@react-spring/web";
 
 import "./FolderList.scss";
+import { FolderItem } from "../folder-item/FolderItem";
 import { FOLDER_TYPE } from "~/core/enums/folder-type.enum";
+import { Board } from "~/models/board.model";
 import { Folder, IFolderResponse } from "~/models/folder.model";
 import { useGetFoldersQuery } from "~/services/api/folders.service";
 
 type FolderListProps = {
   currentFolder: Folder;
   onSelect: (folder: Folder) => void;
-  folderIds: String[];
+  folderIds: string[];
   selectedFolders: Folder[];
-  setFolderIds: React.Dispatch<React.SetStateAction<String[]>>;
+  setFolderIds: React.Dispatch<React.SetStateAction<string[]>>;
   setSelectedFolders: React.Dispatch<React.SetStateAction<Folder[]>>;
+  dragAndDropBoards: Board[];
+  onDragAndDrop: (board: Board) => void;
+  onSetShowModal: (show: boolean) => void;
+  modalProps: any;
+  onSetModalProps: (modalProps: any) => void;
   searchText: string;
 };
 
 export const FolderList: React.FunctionComponent<FolderListProps> = ({
   currentFolder,
   onSelect,
-  folderIds,
-  selectedFolders,
-  setFolderIds,
-  setSelectedFolders,
+  dragAndDropBoards,
+  onDragAndDrop,
+  onSetShowModal,
+  modalProps,
+  onSetModalProps,
   searchText,
 }) => {
-  const { currentApp } = useOdeClient();
   const [foldersQuery, setFoldersQuery] = useState<boolean>(false);
 
   const {
@@ -75,24 +79,6 @@ export const FolderList: React.FunctionComponent<FolderListProps> = ({
     to: { opacity: 1 },
   });
 
-  async function toggleSelect(resource: Folder) {
-    if (folderIds.includes(resource.id)) {
-      setFolderIds(
-        folderIds.filter(
-          (selectedResource: String) => selectedResource !== resource.id,
-        ),
-      );
-      setSelectedFolders(
-        selectedFolders.filter(
-          (selectedResource) => selectedResource.id !== resource.id,
-        ),
-      );
-      return;
-    }
-    setFolderIds([...folderIds, resource.id]);
-    setSelectedFolders([...selectedFolders, resource]);
-  }
-
   useEffect(() => {
     setFoldersQuery(
       currentFolder.id == FOLDER_TYPE.DELETED_BOARDS || !!currentFolder.deleted,
@@ -114,7 +100,7 @@ export const FolderList: React.FunctionComponent<FolderListProps> = ({
               }
             })
             .map((folder: Folder) => {
-              const { id, title } = folder;
+              const { id } = folder;
               return (
                 <animated.li
                   className="g-col-4 z-1 folderSizing"
@@ -124,25 +110,17 @@ export const FolderList: React.FunctionComponent<FolderListProps> = ({
                     ...springs,
                   }}
                 >
-                  <Card
-                    app={currentApp!}
-                    options={{
-                      type: "folder",
-                      title,
-                    }}
-                    isLoading={getFoldersLoading}
-                    isSelectable={true}
-                    isSelected={folderIds.includes(id)}
-                    onSelect={() => toggleSelect(folder)}
-                    onClick={() => {
-                      onSelect(folder);
-                    }}
-                  >
-                    <Card.Body>
-                      <Icon path={mdiFolderPlus} size={1}></Icon>
-                      <Card.Title className="title">{title}</Card.Title>
-                    </Card.Body>
-                  </Card>
+                  <FolderItem
+                    folder={folder}
+                    folders={folderData}
+                    areFoldersLoading={getFoldersLoading}
+                    onSelect={onSelect}
+                    dragAndDropBoards={dragAndDropBoards}
+                    onDragAndDrop={onDragAndDrop}
+                    onDisplayModal={onSetShowModal}
+                    modalData={modalProps}
+                    onSetModalData={onSetModalProps}
+                  />
                 </animated.li>
               );
             })}
--- frontend/src/components/tree-view/TreeViewContainer.tsx
+++ frontend/src/components/tree-view/TreeViewContainer.tsx
@@ -1,23 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./TreeViewContent.scss";
 
-import { TreeView } from "@edifice-ui/react";
+import { TreeView, useOdeClient } from "@edifice-ui/react";
 import { t } from "i18next";
 
-import { FOLDER_TYPE } from "~/core/enums/folder-type.enum";
+import { FOLDER_TYPE, MAIN_PAGE_TITLE } from "~/core/enums/folder-type.enum";
+import { Board } from "~/models/board.model";
 import { FolderTreeNavItem } from "~/models/folder-tree.model";
 import { Folder, IFolderResponse } from "~/models/folder.model";
+import { useMoveBoardsMutation } from "~/services/api/boards.service";
+import { UserRights } from "~/services/utils/share.utils";
 
 type TreeViewContainerProps = {
   folders: Folder[];
   folderObject: FolderTreeNavItem;
   folderType: string;
   onSelect: (folder: Folder) => void;
+  data: any;
+  dispatch: any;
+  dragAndDropBoards: Board[];
+  onDragAndDrop: (board: any) => void;
+  onDisplayModal: (show: boolean) => void;
+  modalData: any;
+  onSetModalData: (modalData: any) => void;
 };
 
 export const TreeViewContainer: React.FunctionComponent<
   TreeViewContainerProps
-> = ({ folders, folderObject, folderType, onSelect }) => {
+> = ({
+  folders,
+  folderObject,
+  folderType,
+  onSelect,
+  data,
+  dispatch,
+  dragAndDropBoards,
+  onDragAndDrop,
+  onDisplayModal,
+  modalData,
+  onSetModalData,
+}) => {
   const dataTree = {
     children: [],
     id: folderType,
@@ -25,8 +47,235 @@ export const TreeViewContainer: React.FunctionComponent<
     section: true,
   };
 
-  const selectFolder = (folderId: string): void => {
-    let clickedFolder: Folder;
+  const [moveBoardsToFolder] = useMoveBoardsMutation();
+  const { user } = useOdeClient();
+  const [userRights] = useState<UserRights>(new UserRights(user));
+
+  const handleDragEnter = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+    dispatch({ type: "SET_DROP_DEPTH", dropDepth: data.dropDepth + 1 });
+  };
+  const handleDragLeave = (e: React.ChangeEvent<HTMLInputElement>) => {
+    removeFolderHighlight(e);
+
+    e.preventDefault();
+    e.stopPropagation();
+    dispatch({ type: "SET_DROP_DEPTH", dropDepth: data.dropDepth - 1 });
+    if (data.dropDepth > 0) return;
+    dispatch({ type: "SET_IN_DROP_ZONE", inDropZone: false });
+  };
+
+  const removeFolderHighlight = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let targetElement: HTMLElement;
+    if (e.target.closest("li")?.id) {
+      if (
+        e.target.closest("li")?.id.split("_")[1] == FOLDER_TYPE.MY_BOARDS ||
+        e.target.closest("li")?.id.split("_")[1] == FOLDER_TYPE.PUBLIC_BOARDS ||
+        e.target.closest("li")?.id.split("_")[1] == FOLDER_TYPE.DELETED_BOARDS
+      ) {
+        targetElement =
+          e.target.closest(".action-container") ?? new HTMLElement();
+      } else {
+        targetElement = e.target.closest("li") ?? new HTMLElement();
+      }
+      targetElement.classList.remove("drag-over");
+    }
+  };
+
+  const handleDragOver = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let targetElement: HTMLElement;
+    if (e.target.closest("li")?.id) {
+      if (
+        e.target.closest("li")?.id.split("_")[1] == FOLDER_TYPE.MY_BOARDS ||
+        e.target.closest("li")?.id.split("_")[1] == FOLDER_TYPE.PUBLIC_BOARDS ||
+        e.target.closest("li")?.id.split("_")[1] == FOLDER_TYPE.DELETED_BOARDS
+      ) {
+        targetElement =
+          e.target.closest(".action-container") ?? new HTMLElement();
+      } else {
+        targetElement = e.target.closest("li") ?? new HTMLElement();
+      }
+      targetElement.classList.add("drag-over");
+    }
+
+    e.preventDefault();
+    e.stopPropagation();
+  };
+
+  const handleDrop = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (
+      !!e.target.closest("li")?.id &&
+      e.target.closest("li")?.id.startsWith("listitem") &&
+      !!e.target.closest("li")?.id.split("_")[1]
+    ) {
+      const targetFolderId = e.target.closest("li")?.id.split("_")[1];
+      const targetFolder: Folder = getFolderData(targetFolderId ?? "");
+      const dragAndDropBoard: Board = dragAndDropBoards[0] ?? new Board();
+      const dragAndDropBoardsIds: string[] = dragAndDropBoards.map(
+        (board: Board) => board._id,
+      );
+      const dragAndDropInitialFolder = !dragAndDropBoard.folderId
+        ? new Folder().build({
+            _id: FOLDER_TYPE.MY_BOARDS,
+            ownerId: user.userId,
+            title: MAIN_PAGE_TITLE,
+            parentId: "",
+          })
+        : folders.find(
+            (folder: Folder) => folder.id == dragAndDropBoard.folderId,
+          ) ?? new Folder();
+
+      if (
+        (!dragAndDropBoards[0] && isOwnerOfSelectedBoards()) ||
+        targetFolderId == FOLDER_TYPE.PUBLIC_BOARDS ||
+        targetFolderId == FOLDER_TYPE.DELETED_BOARDS ||
+        !!targetFolder.deleted
+      ) {
+        //not board owner
+        handleNoRightsDragAndDrop();
+        return;
+      } else if (
+        (userRights.folderOwnerNotShared(dragAndDropInitialFolder) ||
+          userRights.folderOwnerAndSharedOrShareRights(
+            dragAndDropInitialFolder,
+          )) &&
+        userRights.folderOwnerAndSharedOrShareRights(targetFolder)
+      ) {
+        //initial folder owner + not shared or has right + shared, target folder has right + shared
+        confirmSharedFolderDragAndDrop(
+          dragAndDropBoardsIds,
+          targetFolder,
+          "magneto.folder.share.drag.in.warning",
+          targetFolder.title,
+        );
+      } else if (
+        userRights.folderOwnerAndSharedOrShareRights(
+          dragAndDropInitialFolder,
+        ) &&
+        userRights.folderOwnerNotShared(targetFolder)
+      ) {
+        //initial folder has right + shared, target folder owner + not shared
+        confirmSharedFolderDragAndDrop(
+          dragAndDropBoardsIds,
+          targetFolder,
+          "magneto.folder.share.drag.out.warning",
+          dragAndDropInitialFolder.title,
+        );
+      } else if (
+        userRights.folderOwnerNotShared(dragAndDropInitialFolder) &&
+        userRights.folderOwnerNotShared(targetFolder)
+      ) {
+        //initial folder owner + not shared, target folder owner + not shared
+        proceedOnDragAndDrop(dragAndDropBoards, targetFolder);
+      } else {
+        handleNoRightsDragAndDrop();
+      }
+
+      onDragAndDrop(undefined);
+      removeFolderHighlight(e);
+    }
+    e.stopPropagation();
+  };
+
+  const getFolderData = (folderId: string): Folder => {
+    let clickedFolder;
+    if (folderId == FOLDER_TYPE.MY_BOARDS) {
+      clickedFolder = new Folder().build({
+        _id: folderId,
+        title: t("magneto.my.boards"),
+      } as IFolderResponse);
+    } else if (folderId == FOLDER_TYPE.PUBLIC_BOARDS) {
+      clickedFolder = new Folder().build({
+        _id: folderId,
+        title: t("magneto.lycee.connecte.boards"),
+        isPublic: true,
+      } as IFolderResponse);
+    } else if (folderId == FOLDER_TYPE.DELETED_BOARDS) {
+      clickedFolder = new Folder().build({
+        _id: folderId,
+        title: t("magneto.trash"),
+        deleted: true,
+      } as IFolderResponse);
+    } else {
+      clickedFolder =
+        folders.find((folder: Folder) => folder.id == folderId) ?? new Folder();
+    }
+
+    return clickedFolder;
+  };
+
+  const proceedOnDragAndDrop = async (
+    dragAndDropBoards: Board[],
+    dragAndDropTarget: Folder,
+  ) => {
+    const dragAndDropTargetId: string = dragAndDropTarget.id;
+    const dragAndDropBoardsIds: string[] = dragAndDropBoards.map(
+      (board: Board) => board._id,
+    );
+
+    resetDragAndDrop();
+    dragAndDropBoardsCall(dragAndDropBoardsIds, dragAndDropTargetId);
+  };
+
+  const handleNoRightsDragAndDrop = (): void => {
+    onSetModalData({
+      ...modalData,
+      i18nKey: "magneto.folder.drag.drop.right.error",
+      onCancel: () => closeDragAndDropModal(),
+    });
+    onDisplayModal(true);
+  };
+
+  const confirmSharedFolderDragAndDrop = (
+    dragAndDropBoardsIds: string[],
+    dragAndDropTarget: Folder,
+    i18nKey: string,
+    param: string,
+  ) => {
+    onSetModalData({
+      ...modalData,
+      i18nKey: i18nKey,
+      param: param,
+      hasSubmit: true,
+      onSubmit: () =>
+        dragAndDropBoardsCall(dragAndDropBoardsIds, dragAndDropTarget.id),
+      onCancel: () => closeDragAndDropModal(),
+    });
+    onDisplayModal(true);
+  };
+
+  const dragAndDropBoardsCall = (
+    dragAndDropBoardsIds: string[],
+    dragAndDropTargetId: string,
+  ): void => {
+    moveBoardsToFolder({
+      boardIds: dragAndDropBoardsIds,
+      folderId: dragAndDropTargetId,
+    }).catch((e) => {
+      console.log(e);
+    });
+  };
+
+  const isOwnerOfSelectedBoards = (): boolean => {
+    return dragAndDropBoards.every(
+      (board: Board) =>
+        !!board && !!board.owner && board.owner.userId === user.userId,
+    );
+  };
+
+  const closeDragAndDropModal = (): void => {
+    onDisplayModal(false);
+    resetDragAndDrop();
+  };
+
+  const resetDragAndDrop = (): void => {
+    onSelect(new Folder());
+    onDragAndDrop(undefined);
+  };
+
+  const selectFolder = (folderId: string) => {
+    let clickedFolder;
     if (folderId == FOLDER_TYPE.MY_BOARDS) {
       clickedFolder = new Folder().build({
         _id: folderId,
@@ -51,26 +300,38 @@ export const TreeViewContainer: React.FunctionComponent<
     onSelect(clickedFolder);
   };
 
+  useEffect(() => {
+    onSetModalData(modalData);
+  }, [modalData]);
+
   return (
     <>
-      <TreeView
-        data={folderObject ?? dataTree}
-        onTreeItemBlur={() => {
-          console.log("blur");
-        }}
-        onTreeItemFocus={() => {
-          console.log("focus");
-        }}
-        onTreeItemFold={() => {
-          console.log("fold");
-        }}
-        onTreeItemSelect={(item) => {
-          selectFolder(item);
-        }}
-        onTreeItemUnfold={() => {
-          console.log("unfold");
-        }}
-      />
+      <div
+        className={"drag-drop-zone"}
+        onDragOver={handleDragOver}
+        onDragEnter={handleDragEnter}
+        onDragLeave={handleDragLeave}
+        onDrop={handleDrop}
+      >
+        <TreeView
+          data={folderObject ?? dataTree}
+          onTreeItemBlur={() => {
+            console.log("blur");
+          }}
+          onTreeItemFocus={() => {
+            console.log("focus");
+          }}
+          onTreeItemFold={() => {
+            console.log("fold");
+          }}
+          onTreeItemSelect={(item) => {
+            selectFolder(item);
+          }}
+          onTreeItemUnfold={() => {
+            console.log("unfold");
+          }}
+        />
+      </div>
     </>
   );
 };

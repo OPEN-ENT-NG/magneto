c4db08b95cec13b37272476e3674be08a8bd7dc9
ThomasSevagen <thomas.sevagen@cgi.com>
2024-07-09 19:29:53 +0200

    fix(TreeView) : #MAG-390 redo fix icon + initial focus + only one focus (#189)
    * fix(treeView): #MAG-390 icon + default select + unique select fixed + FoldersNavigationProvider
    
    * fix(prettier): #MAG-390 prettier format
    
    * fix(favorites) : #MAG-352 fix magnet favorites visuals (#188)
    
    ---------
    
    Co-authored-by: FlorentMr <florent.mariotti@cgi.com>
    
    * fix(treeView): #MAG-390 icon + default select + unique select fixed + FoldersNavigationProvider
    
    * fix(prettier): #MAG-390 prettier format
    
    * feat(dragAndDrop): #MAG-392 implement drag and drop (#190)
    
    * feat(share) : #MAG-350 add share modal to let other ppl access to folders or boards. (#192)
    
    Co-authored-by: FlorentMr <florent.mariotti@cgi.com>
    
    * fix(re7) : #MAG-391-redo fix all +FolderTitle cpt (#191)
    
    * fix(re7) : #MAG-391-redo fix all +FolderTitle cpt
    
    * fix(prettier): #MAG-391-redo prettier format
    
    * fix(re7) : #MAG-391-redo fix all +FolderTitle cpt
    
    * fix(prettier): #MAG-391-redo prettier format
    
    * fix(re7) : #MAG-391-redo fix all +FolderTitle cpt
    
    * fix(prettier): #MAG-391-redo prettier format
    
    * fix(re7) : #MAG-391-redo fix all +FolderTitle cpt
    
    * fix(prettier): #MAG-391-redo prettier format
    
    * fix(treeView): #MAG-390 icon + default select + unique select fixed + FoldersNavigationProvider
    
    * fix(prettier): #MAG-390 prettier format
    
    * fix(treeView): #MAG-390 icon + default select + unique select fixed + FoldersNavigationProvider
    
    * fix(prettier): #MAG-390 prettier format
    
    * oui
    
    * prettier
    
    ---------
    
    Co-authored-by: jinhda.douang <jinhda.douangphrachanh@cgi.com>
    Co-authored-by: Florent Mariotti <florentcorse@gmail.com>
    Co-authored-by: FlorentMr <florent.mariotti@cgi.com>
    Co-authored-by: alicedraillard <85180705+alicedraillard@users.noreply.github.com>

---
+++ frontend/src/providers/FoldersNavigationProvider/utils.ts
@@ -0,0 +1,44 @@
+import { t } from "i18next";
+
+import { FolderObjectState, TriggerFetchState } from "./types";
+import { FOLDER_TYPE } from "~/core/enums/folder-type.enum";
+import { Folder, IFolderResponse } from "~/models/folder.model";
+
+export const initialCurrentFolder: Folder = new Folder(FOLDER_TYPE.MY_BOARDS);
+
+export const initialFolderObject: FolderObjectState = {
+  myFolderObject: null,
+  deletedFolderObject: null,
+};
+
+export const initialTriggerFetch: TriggerFetchState = {
+  myFolders: false,
+  deletedFolders: false,
+};
+
+//prepareFolders non utilisé pour l'instant
+export const prepareFolders = (folderId: string, folders: Folder[]): Folder => {
+  switch (folderId) {
+    case FOLDER_TYPE.MY_BOARDS:
+      return new Folder().build({
+        _id: folderId,
+        title: t("magneto.my.boards"),
+      } as IFolderResponse);
+    case FOLDER_TYPE.PUBLIC_BOARDS:
+      return new Folder().build({
+        _id: folderId,
+        title: t("magneto.lycee.connecte.boards"),
+        isPublic: true,
+      } as IFolderResponse);
+    case FOLDER_TYPE.DELETED_BOARDS:
+      return new Folder().build({
+        _id: folderId,
+        title: t("magneto.trash"),
+        deleted: true,
+      } as IFolderResponse);
+    default:
+      return (
+        folders.find((folder: Folder) => folder.id === folderId) ?? new Folder()
+      );
+  }
+};

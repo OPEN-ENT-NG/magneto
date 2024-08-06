d1df60fca9c71bfdbcd1dc48ae4e98462d93cdaf
alicedraillard <85180705+alicedraillard@users.noreply.github.com>
2024-08-06 16:51:08 +0200

    fix(search): #MAG-327 handle undefined value (#233)

--- frontend/src/components/board-list/utils.ts
+++ frontend/src/components/board-list/utils.ts
@@ -3,11 +3,11 @@ import { Board } from "~/models/board.model";
 export const isBoardInFilter = (board: Board, searchText: string) => {
   if (
     board.title.toLowerCase().includes(searchText.toLowerCase()) ||
-    (board.description !== null &&
+    (!!board.description &&
       board.description.toLowerCase().includes(searchText.toLowerCase()))
   )
     return true;
-  if (board.tags === null || board.tags.length === 0) {
+  if (board.tags === null || !board.tags || board.tags.length === 0) {
     return false;
   }
   return board.tags.some((tag) =>

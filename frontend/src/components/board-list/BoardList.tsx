4dc76a451e07ee7066f3f5490133607c92e2cb23
FlorentMr <florent.mariotti@cgi.com>
2024-08-08 16:35:40 +0200

    fix(loading): #MAG-424 fix loading on fetch board

--- frontend/src/components/board-list/BoardList.tsx
+++ frontend/src/components/board-list/BoardList.tsx
@@ -7,6 +7,7 @@ import { isBoardInFilter } from "./utils";
 import { BoardItem } from "~/components/board-item/BoardItem";
 import { Board } from "~/models/board.model";
 import { useBoardsNavigation } from "~/providers/BoardsNavigationProvider";
+import { Loading } from "@edifice-ui/react";
 
 type BoardListProps = {
   onDragAndDrop: (board: Board) => void;
@@ -22,43 +23,49 @@ export const BoardList: React.FunctionComponent<BoardListProps> = ({
     to: { opacity: 1 },
   });
 
-  const { boards } = useBoardsNavigation();
+  const { boards, boardsLoading } = useBoardsNavigation();
   const [isBoardDragged, setIsBoardDragged] = useState<boolean>(false);
 
   return (
     <>
-      {boards?.length ? (
-        <animated.ul className="grid ps-0 list-unstyled mb-24">
-          {boards
-            .filter((board: Board) => {
-              if (searchText === "") {
-                return board;
-              } else if (isBoardInFilter(board, searchText)) {
-                return board;
-              }
-            })
-            .map((board: Board) => {
-              const { id } = board;
-              return (
-                <animated.li
-                  className="g-col-4 z-1 boardSizing"
-                  key={id}
-                  style={{
-                    position: "relative",
-                    ...springs,
-                  }}
-                >
-                  <BoardItem
-                    board={board}
-                    onDragAndDropBoard={onDragAndDrop}
-                    isBoardDragged={isBoardDragged}
-                    setIsBoardDragged={setIsBoardDragged}
-                  />
-                </animated.li>
-              );
-            })}
-        </animated.ul>
-      ) : null}
+      {boardsLoading ? (
+        <Loading isLoading loadingPosition="left">
+          Loading...
+        </Loading>
+      ) : (
+        boards?.length > 0 && (
+          <animated.ul className="grid ps-0 list-unstyled mb-24">
+            {boards
+              .filter((board: Board) => {
+                if (searchText === "") {
+                  return board;
+                } else if (isBoardInFilter(board, searchText)) {
+                  return board;
+                }
+              })
+              .map((board: Board) => {
+                const { id } = board;
+                return (
+                  <animated.li
+                    className="g-col-4 z-1 boardSizing"
+                    key={id}
+                    style={{
+                      position: "relative",
+                      ...springs,
+                    }}
+                  >
+                    <BoardItem
+                      board={board}
+                      onDragAndDropBoard={onDragAndDrop}
+                      isBoardDragged={isBoardDragged}
+                      setIsBoardDragged={setIsBoardDragged}
+                    />
+                  </animated.li>
+                );
+              })}
+          </animated.ul>
+        )
+      )}
     </>
   );
 };

ca70ec225588eade93c7c5e9b63e7a011f6e213a
alice.draillard <alice.draillard@cgi.com>
2024-08-16 17:03:16 +0200

    YEAY

--- frontend/src/components/share-modal/ShareModalMagneto.tsx
+++ frontend/src/components/share-modal/ShareModalMagneto.tsx
@@ -1,10 +1,11 @@
 import { FunctionComponent } from "react";
 
-import { OdeClientProvider, ShareModal } from "@edifice-ui/react";
+import { OdeClientProvider } from "@edifice-ui/react";
 import { useOdeClient } from "@edifice-ui/react";
 import { RightStringified } from "edifice-ts-client";
 
 import { RESOURCE_BIG_TYPE } from "~/core/enums/resource-big-type.enum";
+import { ShareModal } from "~/common/ShareModal";
 
 type props = {
   isOpen: boolean;

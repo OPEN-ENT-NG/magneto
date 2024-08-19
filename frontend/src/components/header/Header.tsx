5eb82c0ef6828590af61846b6e36c1bfa7435978
alicedraillard <85180705+alicedraillard@users.noreply.github.com>
2024-08-19 14:59:34 +0200

    fix(share): #MAG-350 fix share rights and view (#243)

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,10 +1,15 @@
-import { AppHeader, Button } from "@edifice-ui/react";
+import { useEffect, useState } from "react";
+
+import { AppHeader, Button, useOdeClient } from "@edifice-ui/react";
 import "./Header.scss";
 import MenuIcon from "@mui/icons-material/Menu";
 import IconButton from "@mui/material/IconButton";
 import { useTranslation } from "react-i18next";
 
 import myimg from "./uni-magneto.png";
+import { FOLDER_TYPE } from "~/core/enums/folder-type.enum";
+import { useFoldersNavigation } from "~/providers/FoldersNavigationProvider";
+import { UserRights } from "~/utils/share.utils";
 
 interface HeaderProps {
   onClick: () => void;
@@ -13,6 +18,20 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = ({ onClick, toggleDrawer }) => {
   const { t } = useTranslation("magneto");
+  const { user } = useOdeClient();
+  const { currentFolder } = useFoldersNavigation();
+  const [isFolderOwnerOrSharedWithRights, setIsFolderOwnerOrSharedWithRights] =
+    useState<boolean>(false);
+  const [userRights] = useState<UserRights>(new UserRights(user));
+
+  useEffect(() => {
+    setIsFolderOwnerOrSharedWithRights(
+      currentFolder.id == FOLDER_TYPE.MY_BOARDS ||
+        currentFolder.ownerId === user?.userId ||
+        userRights.folderHasShareRights(currentFolder, "publish"),
+    );
+  }, [currentFolder]);
+
   return (
     <AppHeader>
       <IconButton
@@ -28,15 +47,17 @@ const Header: React.FC<HeaderProps> = ({ onClick, toggleDrawer }) => {
         <img src={myimg} alt="Logo" className="logo" />
         <span className="header-text">{t("magneto.header.my.boards")}</span>
       </div>
-      <Button
-        color="primary"
-        type="button"
-        variant="filled"
-        onClick={onClick}
-        className="button"
-      >
-        Créer un tableau
-      </Button>
+      {isFolderOwnerOrSharedWithRights && (
+        <Button
+          color="primary"
+          type="button"
+          variant="filled"
+          onClick={onClick}
+          className="button"
+        >
+          {t("magneto.create.board")}
+        </Button>
+      )}
     </AppHeader>
   );
 };

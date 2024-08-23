83c946a0cfc113667868fe9a3b93101a8c14fc06
alicedraillard <85180705+alicedraillard@users.noreply.github.com>
2024-08-23 17:16:56 +0200

    fix(rights): #MAG-392 fix dragAndDrop and moveModal rights (#249)

--- frontend/src/components/move-board/MoveBoard.tsx
+++ frontend/src/components/move-board/MoveBoard.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useState } from "react";
+import { FunctionComponent, useState } from "react";
 
 // eslint-disable-next-line
 import { Button, Modal, TreeView, useOdeClient } from "@edifice-ui/react";
@@ -8,37 +8,37 @@ import { useTranslation } from "react-i18next";
 import { useGetFolderTypeData } from "../tree-view/utils";
 import { FOLDER_TYPE } from "~/core/enums/folder-type.enum";
 import { Board } from "~/models/board.model";
-import { FolderTreeNavItem } from "~/models/folder-tree.model";
 import { Folder, IFolderResponse } from "~/models/folder.model";
+import { useBoardsNavigation } from "~/providers/BoardsNavigationProvider";
 import { useFoldersNavigation } from "~/providers/FoldersNavigationProvider";
 import { useMoveBoardsMutation } from "~/services/api/boards.service";
-import { useGetFoldersQuery } from "~/services/api/folders.service";
+import { UserRights } from "~/utils/share.utils";
 
 type props = {
   isOpen: boolean;
   toggle: () => void;
-  boards: Board[];
   reset: () => void;
+  onDisplayModal: (show: boolean) => void;
+  modalData: any;
+  onSetModalData: (modalData: any) => void;
 };
 
 export const MoveBoard: FunctionComponent<props> = ({
   isOpen,
   toggle,
-  boards,
   reset,
+  onDisplayModal,
+  modalData,
+  onSetModalData,
 }: props) => {
   const { t } = useTranslation("magneto");
+  const { user } = useOdeClient();
+  const [userRights] = useState<UserRights>(new UserRights(user));
   const [moveBoards] = useMoveBoardsMutation();
   const [selectedFolderId, setSelectedFolderId] = useState<string>("");
-  const { folderObject, folderNavigationRefs } = useFoldersNavigation();
-
-  const { user } = useOdeClient();
-
-  const userId = user ? user?.userId : "";
-
-  const isSameAsUser = (id: string) => {
-    return id == userId;
-  };
+  const { currentFolder, folderData, folderObject, folderNavigationRefs } =
+    useFoldersNavigation();
+  const { selectedBoards, selectedBoardsIds } = useBoardsNavigation();
 
   const dataTree = {
     children: [],
@@ -48,42 +48,151 @@ export const MoveBoard: FunctionComponent<props> = ({
     isPublic: false,
   };
 
-  const {
-    data: myFoldersResult,
-    isLoading: getFoldersLoading,
-    error: getFoldersError,
-  } = useGetFoldersQuery(false);
+  const getFolderData = (folderId: string): Folder => {
+    let clickedFolder;
+    if (folderId == FOLDER_TYPE.MY_BOARDS) {
+      clickedFolder = new Folder().build({
+        _id: folderId,
+        title: t("magneto.my.boards"),
+      } as IFolderResponse);
+    } else if (folderId == FOLDER_TYPE.PUBLIC_BOARDS) {
+      clickedFolder = new Folder().build({
+        _id: folderId,
+        title: t("magneto.lycee.connecte.boards"),
+        isPublic: true,
+      } as IFolderResponse);
+    } else if (folderId == FOLDER_TYPE.DELETED_BOARDS) {
+      clickedFolder = new Folder().build({
+        _id: folderId,
+        title: t("magneto.trash"),
+        deleted: true,
+      } as IFolderResponse);
+    } else {
+      clickedFolder =
+        folderData.find((folder: Folder) => folder.id == folderId) ??
+        new Folder();
+    }
 
-  let myFolders: Folder[] = [];
-  let myFoldersObject: FolderTreeNavItem | undefined = undefined;
+    return clickedFolder;
+  };
 
-  if (!getFoldersError && !getFoldersLoading) {
-    myFolders = myFoldersResult.map((folder: IFolderResponse) =>
-      new Folder().build(folder),
-    ); //convert folders to Folder[]
+  const moveBoardsCall = (
+    dragAndDropBoardsIds: string[],
+    dragAndDropTargetId: string,
+  ): void => {
+    moveBoards({
+      boardIds: dragAndDropBoardsIds,
+      folderId: dragAndDropTargetId,
+    }).catch((e) => {
+      console.log(e);
+    });
+  };
 
-    myFoldersObject = new FolderTreeNavItem({
-      id: FOLDER_TYPE.MY_BOARDS,
-      title: t("magneto.my.boards"),
-      parentId: "",
-      section: true,
-    }).buildFolders(myFolders);
-  }
+  const closeMoveRightsModal = (): void => {
+    onDisplayModal(false);
+  };
+
+  const proceedOnMove = async (targetFolder: Folder) => {
+    const targetFolderId: string = targetFolder.id;
+
+    moveBoards({
+      boardIds: selectedBoardsIds,
+      folderId: targetFolderId,
+    }).catch((e) => {
+      console.log(e);
+    });
+  };
+
+  const handleNoMoveRights = (): void => {
+    onSetModalData({
+      ...modalData,
+      i18nKey: "magneto.folder.drag.drop.right.error",
+      onCancel: () => closeMoveRightsModal(),
+      hasSubmit: false,
+    });
+    toggle();
+    onDisplayModal(true);
+  };
+
+  const confirmSharedFolderMove = (
+    targetFolder: Folder,
+    i18nKey: string,
+    param: string,
+  ) => {
+    onSetModalData({
+      ...modalData,
+      i18nKey: i18nKey,
+      param: param,
+      hasSubmit: true,
+      onSubmit: () => {
+        closeMoveRightsModal();
+        moveBoardsCall(selectedBoardsIds, targetFolder.id);
+      },
+      onCancel: () => closeMoveRightsModal(),
+    });
+    toggle();
+    onDisplayModal(true);
+  };
+
+  const isOwnerOfSelectedBoards = (): boolean => {
+    return (
+      selectedBoards.filter(
+        (board: Board) => board?.owner?.userId === user?.userId,
+      ).length == selectedBoards.length
+    );
+  };
+
+  const handleMoveRights = () => {
+    if (selectedBoards.length) {
+      const destinationFolder = getFolderData(selectedFolderId);
+
+      if (
+        (!!selectedBoards[0] && !isOwnerOfSelectedBoards()) ||
+        destinationFolder.id == FOLDER_TYPE.PUBLIC_BOARDS ||
+        destinationFolder.id == FOLDER_TYPE.DELETED_BOARDS ||
+        !!destinationFolder.deleted
+      ) {
+        //not board owner
+        handleNoMoveRights();
+      } else if (
+        (userRights.folderOwnerNotShared(currentFolder) ||
+          userRights.folderOwnerAndSharedOrShareRights(currentFolder)) &&
+        userRights.folderOwnerAndSharedOrShareRights(destinationFolder)
+      ) {
+        //initial folder owner + not shared or has right + shared, target folder has right + shared
+        confirmSharedFolderMove(
+          destinationFolder,
+          "magneto.folder.share.drag.in.warning",
+          destinationFolder.title,
+        );
+      } else if (
+        userRights.folderOwnerAndSharedOrShareRights(currentFolder) &&
+        userRights.folderOwnerNotShared(destinationFolder)
+      ) {
+        //initial folder has right + shared, target folder owner + not shared
+        confirmSharedFolderMove(
+          destinationFolder,
+          "magneto.folder.share.drag.out.warning",
+          currentFolder.title,
+        );
+      } else if (
+        userRights.folderOwnerNotShared(currentFolder) &&
+        userRights.folderOwnerNotShared(destinationFolder) &&
+        isOwnerOfSelectedBoards()
+      ) {
+        //initial folder owner + not shared, target folder owner + not shared + selected boards are ours
+        proceedOnMove(destinationFolder);
+        toggle();
+      } else {
+        handleNoMoveRights();
+      }
+
+      reset();
+    }
+  };
 
   const onSubmit = (): void => {
-    const boardIds: string[] = boards
-      .filter((board) => {
-        return isSameAsUser(board.owner.userId);
-      })
-      .map((myBoard) => {
-        return myBoard.id;
-      });
-    moveBoards({
-      boardIds: boardIds,
-      folderId: selectedFolderId,
-    });
-    reset();
-    toggle();
+    handleMoveRights();
   };
 
   const datas = useGetFolderTypeData(FOLDER_TYPE.MY_BOARDS, folderObject);
@@ -102,7 +211,7 @@ export const MoveBoard: FunctionComponent<props> = ({
             <h2>{t("magneto.board.move")}</h2>
           </Modal.Header>
           <Modal.Body>
-            {myFoldersObject && (
+            {(datas || dataTree) && (
               <TreeView
                 ref={folderNavigationRefs[FOLDER_TYPE.MY_BOARDS]}
                 data={datas || dataTree}

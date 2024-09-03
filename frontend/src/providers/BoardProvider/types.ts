8419487dcf04c186f772b7ec7925a4fd3372aceb
Florent Mariotti <florentcorse@gmail.com>
2024-09-03 14:29:39 +0200

    feat(header) : #MAG-438 add header and setup for the board view (#261)
    ---------
    
    Co-authored-by: FlorentMr <florent.mariotti@cgi.com>
    Co-authored-by: thomas.sevagen <thomas.sevagen@cgi.com>

---
+++ frontend/src/providers/BoardProvider/types.ts
@@ -0,0 +1,27 @@
+import { Dispatch, ReactNode, SetStateAction } from "react";
+
+import { Board } from "~/models/board.model";
+import { Card } from "~/models/card.model";
+
+export interface BoardProviderProps {
+  children: ReactNode;
+}
+
+export type BoardContextType = {
+  board: Board;
+  setBoard: Dispatch<SetStateAction<Board>>;
+};
+
+export type Section = {
+  id: string;
+  title: string;
+  cardIds: string[];
+  boardId: string;
+  page: number;
+  cards: Card[];
+  displayed?: boolean;
+};
+
+export type Sections = {
+  all: Section[];
+};

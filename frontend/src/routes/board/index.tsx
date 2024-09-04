42d01edc6d2c1ee66f2f1194e578cb5a208b9aeb
alicedraillard <85180705+alicedraillard@users.noreply.github.com>
2024-09-04 09:49:28 +0200

    feat(sideMenu): #MAG-338 add side menu display (#260)

--- frontend/src/routes/board/index.tsx
+++ frontend/src/routes/board/index.tsx
@@ -1,10 +1,15 @@
 import { HeaderView } from "~/components/header-view/HeaderView";
+import { SideMenu } from "~/components/side-menu/SideMenu";
+import { useSideMenuData } from "~/hooks/useSideMenuData";
 import { BoardProvider } from "~/providers/BoardProvider";
 
 export const App = () => {
+  const sideMenuData = useSideMenuData();
+
   return (
     <BoardProvider>
       <HeaderView />
+      <SideMenu sideMenuData={sideMenuData} />
     </BoardProvider>
   );
 };

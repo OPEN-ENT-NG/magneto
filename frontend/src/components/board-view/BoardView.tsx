43a0b001ee090b36c0cd65094a1b4d3be7399502
alicedraillard <85180705+alicedraillard@users.noreply.github.com>
2024-09-05 15:13:07 +0200

    feat(zoom): #MAG-407 add zoom feature (#263)

---
+++ frontend/src/components/board-view/BoardView.tsx
@@ -0,0 +1,30 @@
+import { FC } from "react";
+
+import "./BoardView.scss";
+
+import { SideMenu } from "../side-menu/SideMenu";
+import { ZoomComponent } from "../zoom-component/ZoomComponent";
+import { useSideMenuData } from "~/hooks/useSideMenuData";
+import { useBoard } from "~/providers/BoardProvider";
+
+export const BoardView: FC = () => {
+  const sideMenuData = useSideMenuData();
+  const { zoomLevel, zoomIn, zoomOut, resetZoom } = useBoard();
+
+  return (
+    <>
+      <SideMenu sideMenuData={sideMenuData} />
+
+      <div className="zoom-container">
+        <ZoomComponent
+          opacity={0.75}
+          zoomLevel={zoomLevel}
+          zoomMaxLevel={5}
+          zoomIn={zoomIn}
+          zoomOut={zoomOut}
+          resetZoom={resetZoom}
+        />
+      </div>
+    </>
+  );
+};

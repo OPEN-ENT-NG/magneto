7325796c42fd75e6cfafe7451f8c710d4373e452
thomas.sevagen <thomas.sevagen@cgi.com>
2024-09-06 17:33:19 +0200

    fix header style+hourToast+button

--- frontend/src/components/header-view/HeaderView.tsx
+++ frontend/src/components/header-view/HeaderView.tsx
@@ -1,17 +1,66 @@
-import React from "react";
+import { FC } from "react";
 
-import { AppHeader, Breadcrumb, useOdeClient } from "@edifice-ui/react";
+import { AppHeader, Breadcrumb, Button, useOdeClient } from "@edifice-ui/react";
+import { mdiCheckCircle } from "@mdi/js";
+import Icon from "@mdi/react";
+import "./HeaderView.scss";
+import { Box } from "@mui/material";
+import { useTranslation } from "react-i18next";
 
+import { toastStyle } from "./style";
 import { useBoard } from "~/providers/BoardProvider";
 
-export const HeaderView: React.FunctionComponent = () => {
+export const HeaderView: FC = () => {
   const { board } = useBoard();
-
   const { currentApp } = useOdeClient();
+  const { t } = useTranslation("magneto");
+  console.log(board);
+  const modificationDate = board.modificationDate.split(" ")[0];
+  const modificationHour = board.modificationDate.split(" ")[1];
+  const onClick = () => console.log("read clicked");
 
   return (
     <AppHeader>
-      {currentApp && <Breadcrumb app={currentApp} name={board?.title} />}
+      <Box
+        sx={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          boxSizing: "border-box",
+          padding: "0 1rem 1rem 1rem",
+        }}
+      >
+        {currentApp && <Breadcrumb app={currentApp} name={board?.title} />}
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            boxSizing: "border-box",
+            gap: "3.5rem",
+          }}
+        >
+          <Box sx={toastStyle}>
+            <Icon path={mdiCheckCircle} size={1} />
+            <span>
+              {t("magneto.board.saved.at", {
+                0: modificationDate,
+                1: modificationHour,
+              })}
+            </span>
+          </Box>
+          <Button
+            color="primary"
+            type="button"
+            variant="filled"
+            onClick={onClick}
+            className="button"
+          >
+            {t("magneto.read")}
+          </Button>
+        </Box>
+      </Box>
     </AppHeader>
   );
 };
---
+++ frontend/src/components/header-view/style.ts
@@ -0,0 +1,10 @@
+export const toastStyle = {
+  display: "flex",
+  alignItems: "center",
+  gap: ".5rem",
+  background: "#bde4f9",
+  padding: " .8rem .5rem",
+  fontSize: "1.5rem",
+  borderRadius: ".5rem",
+  color: "#2a9cc8",
+};

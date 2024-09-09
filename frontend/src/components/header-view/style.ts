a94e8fa83c1f8d68498e01d9018bdc4f3d0246b1
Thomas Sevagen <thomas.sevagen@cgi.com>
2024-09-09 09:38:13 +0200

    fix(headerview): #MAG-438 fix header style+hourToast+button (#267)
    * fix header style+hourToast+button
    
    * ext style
    
    * fix style

---
+++ frontend/src/components/header-view/style.ts
@@ -0,0 +1,27 @@
+export const toastStyle = {
+  display: "flex",
+  alignItems: "center",
+  gap: ".5rem",
+  background: "#bde4f9",
+  padding: " .8rem .5rem",
+  fontSize: "1.5rem",
+  borderRadius: ".5rem",
+  color: "#2a9cc8",
+};
+
+export const wrapperBoxStyle = {
+  width: "100%",
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+  boxSizing: "border-box",
+  padding: "0 1rem",
+};
+
+export const leftWrapperStyle = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  boxSizing: "border-box",
+  gap: "3.5rem",
+};

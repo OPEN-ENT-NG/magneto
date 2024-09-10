49fc48e5d18af92496f4829bdb2cff4977b26c09
Thomas Sevagen <thomas.sevagen@cgi.com>
2024-09-10 11:33:37 +0200

    feat(description): #MAG-439 description+modal (#268)
    * WIP
    
    * Header WIP
    
    * feat(description): #MAG-439 description+modal
    
    * fix some style
    
    ---------
    
    Co-authored-by: FlorentMr <florent.mariotti@cgi.com>

---
+++ frontend/src/components/board-description/style.ts
@@ -0,0 +1,41 @@
+import { styled, Typography } from "@mui/material";
+
+import { StyledTypographyProps } from "./types";
+
+export const StyledTypography = styled(Typography)<StyledTypographyProps>(
+  ({ isOverflowing }) => ({
+    maxWidth: "100%",
+    overflow: "hidden",
+    whiteSpace: "pre-wrap",
+    wordWrap: "break-word",
+    overflowWrap: "break-word",
+    maxHeight: "7rem",
+    fontSize: "1.5rem",
+    lineHeight: "23px",
+    color: "#5b6472",
+    ...(isOverflowing && {
+      maskImage: "linear-gradient(to bottom, black 60%, transparent 100%)",
+      WebkitMaskImage:
+        "linear-gradient(to bottom, black 60%, transparent 100%)",
+    }),
+  }),
+);
+
+export const buttonStyle = {
+  alignSelf: "flex-start",
+  color: "#2A9CC8",
+  fontSize: "1.2rem",
+  fontWeight: "bolder",
+  fontFamily: "roboto",
+  padding: 0,
+  "&:hover": {
+    color: "#FF8500",
+    backgroundColor: "transparent",
+  },
+};
+
+export const boardDescriptionWrapperSyle = {
+  width: "100%",
+  display: "flex",
+  flexDirection: "column",
+};
---
+++ frontend/src/components/board-description/types.ts
@@ -0,0 +1,3 @@
+export interface StyledTypographyProps {
+  isOverflowing: boolean;
+}

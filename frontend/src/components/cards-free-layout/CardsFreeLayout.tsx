95818bd6380aa8a38ccfbdd30fb1579d85d1b2b4
alice.draillard <alice.draillard@cgi.com>
2024-09-10 11:32:05 +0200

    position + card size (not working)

--- frontend/src/components/cards-free-layout/CardsFreeLayout.tsx
+++ frontend/src/components/cards-free-layout/CardsFreeLayout.tsx
@@ -1,15 +1,17 @@
 import { animated } from "@react-spring/web";
 import { FC } from "react";
 import { Card } from "~/models/card.model";
-import { LiWrapper, UlWrapper, cardBoxStyle } from "./style";
+import { CardBoxStyle, LiWrapper, UlWrapper, cardBoxStyle } from "./style";
 import { useBoard } from "~/providers/BoardProvider";
 import { Box } from "@mui/material";
 
 
 export const CardsFreeLayout: FC = () => {
-    const { board } = useBoard();
+    const { board, zoomLevel } = useBoard();
 
     // const board = {cards: [{title: "1"},{title: "2"},{title: "3"}]}
+
+    console.log(zoomLevel)
 
   return (
     <>
@@ -21,7 +23,7 @@ export const CardsFreeLayout: FC = () => {
                 <LiWrapper isFirst={false}>
 
                 
-                  <Box sx={cardBoxStyle}>{card.title}</Box>
+                  <CardBoxStyle zoomLevel={zoomLevel}>{card.title}</CardBoxStyle>
                 </LiWrapper>
 
               );

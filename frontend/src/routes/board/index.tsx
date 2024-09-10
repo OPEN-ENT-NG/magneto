20d3c44008888157c4afdaaaed49386e62801b71
alicedraillard <85180705+alicedraillard@users.noreply.github.com>
2024-09-10 17:43:17 +0200

    feat(freeLayout): #MAG-446 implement free layout + rearrange view (#270)

--- frontend/src/routes/board/index.tsx
+++ frontend/src/routes/board/index.tsx
@@ -1,11 +1,9 @@
 import { BoardView } from "~/components/board-view/BoardView";
-import { HeaderView } from "~/components/header-view/HeaderView";
 import { BoardProvider } from "~/providers/BoardProvider";
 
 export const App = () => {
   return (
     <BoardProvider>
-      <HeaderView />
       <BoardView />
     </BoardProvider>
   );

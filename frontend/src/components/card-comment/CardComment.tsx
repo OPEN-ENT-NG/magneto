fc4d0e94f36c150be69c14945e201ccd45152688
Thomas Sevagen <thomas.sevagen@cgi.com>
2024-09-16 14:45:58 +0200

    Add commentary (#281)
    * wip
    
    * wip
    
    * wip
    
    * fix errors
    
    * prettier

---
+++ frontend/src/components/card-comment/CardComment.tsx
@@ -0,0 +1,86 @@
+import { FC, useState, KeyboardEvent } from "react";
+
+import { useUser } from "@edifice-ui/react";
+import { mdiCommentOutline } from "@mdi/js";
+import Icon from "@mdi/react";
+import { Avatar, Box, InputBase, Typography } from "@mui/material";
+import { useTranslation } from "react-i18next";
+
+import {
+  avatarStyle,
+  commentContentContainerStyle,
+  commentCountContainerStyle,
+  commentCountTypographyStyle,
+  commentTextContainerStyle,
+  commentTextStyle,
+  containerStyle,
+  inputBaseStyle,
+  inputContainerStyle,
+  timeStyle,
+  userNameStyle,
+} from "./style";
+import { CardCommentProps } from "./types";
+import { useElapsedTime } from "~/hooks/useElapsedTime";
+import { useAddCommentMutation } from "~/services/api/comment.service";
+
+export const CardComment: FC<CardCommentProps> = ({ commentData }) => {
+  const [inputValue, setInputValue] = useState<string>("");
+  const [addComment] = useAddCommentMutation();
+  const { t } = useTranslation("magneto");
+  const { user, avatar } = useUser();
+  const { cardComment, nbOfComment, cardId } = commentData;
+
+  const time = useElapsedTime(cardComment.modificationDate);
+
+  const handleSubmit = async (event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter" && inputValue && cardId) {
+      try {
+        await addComment({
+          cardId: cardId,
+          content: inputValue,
+        }).unwrap();
+        setInputValue("");
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+
+  return (
+    <Box sx={containerStyle}>
+      {cardComment && nbOfComment > 0 && (
+        <>
+          <Box sx={commentCountContainerStyle}>
+            <Icon path={mdiCommentOutline} size={1} />
+            <Typography sx={commentCountTypographyStyle}>
+              {nbOfComment}
+            </Typography>
+          </Box>
+          <Box sx={commentContentContainerStyle}>
+            <Avatar sx={avatarStyle} src={avatar}></Avatar>
+            <Box sx={commentTextContainerStyle}>
+              <Typography sx={userNameStyle}>
+                {`${user?.firstName} ${user?.lastName}`}
+              </Typography>
+              <Typography sx={timeStyle}>{time?.label}</Typography>
+              <Typography sx={commentTextStyle}>
+                {cardComment.content}
+              </Typography>
+            </Box>
+          </Box>
+        </>
+      )}
+      <Box sx={inputContainerStyle}>
+        <Avatar sx={avatarStyle} src={avatar}></Avatar>
+        <InputBase
+          placeholder={t("magneto.add.comment")}
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          onKeyDown={handleSubmit}
+          fullWidth
+          sx={inputBaseStyle}
+        />
+      </Box>
+    </Box>
+  );
+};

498a1f89436d0db7dd852ff060fe0643c4a55be1
FlorentMr <florent.mariotti@cgi.com>
2024-09-18 11:50:47 +0200

    fix(card) : #MAG-452 fix style and fix legend missing

---
+++ frontend/src/components/tooltip/Tooltip.tsx
@@ -0,0 +1,25 @@
+import { FC } from "react";
+
+import { Tooltip as TooltipMUI } from "@mui/material";
+import { TooltipProps } from "./types";
+
+export const Tooltip: FC<TooltipProps> = ({ children, title, placement }) => {
+  return (
+    <TooltipMUI
+      title={title}
+      {...(placement && { placement })}
+      TransitionProps={{
+        style: { marginTop: "-0.4rem" },
+      }}
+      componentsProps={{
+        tooltip: {
+          sx: {
+            fontSize: "1.4rem",
+          },
+        },
+      }}
+    >
+      {children}
+    </TooltipMUI>
+  );
+};

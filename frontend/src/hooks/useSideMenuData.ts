aefdf9f7c55dd329f3301140d35ecb305ffa129d
FlorentMr <florent.mariotti@cgi.com>
2024-10-02 16:22:56 +0200

    add enums

--- frontend/src/hooks/useSideMenuData.ts
+++ frontend/src/hooks/useSideMenuData.ts
@@ -18,6 +18,7 @@ import {
   SideMenuDividerProp,
 } from "~/models/side-menu.model";
 import { useMediaLibrary } from "~/providers/MediaLibraryProvider";
+import { MEDIA_LIBRARY_TYPE } from "~/core/enums/media-library-type.enum";
 
 export const useSideMenuData = (): (
   | SideMenuIconProp
@@ -30,34 +31,32 @@ export const useSideMenuData = (): (
     {
       icon: createElement(Icon, { path: mdiFormatSize, size: 1.5 }),
       name: t("magneto.card.type.text"),
-      action: () => {
-        console.log("text");
-      },
+      action: () => console.log("text"),
     },
     {
       icon: createElement(Icon, { path: mdiImage, size: 1.5 }),
       name: t("magneto.card.type.image"),
-      action: () => handleClickMedia("image"),
+      action: () => handleClickMedia(MEDIA_LIBRARY_TYPE.IMAGE),
     },
     {
       icon: createElement(Icon, { path: mdiPlayCircle, size: 1.5 }),
       name: t("magneto.card.type.video"),
-      action: () => handleClickMedia("video"),
+      action: () => handleClickMedia(MEDIA_LIBRARY_TYPE.VIDEO),
     },
     {
       icon: createElement(Icon, { path: mdiMusicNote, size: 1.5 }),
       name: t("magneto.card.type.audio"),
-      action: () => handleClickMedia("audio"),
+      action: () => handleClickMedia(MEDIA_LIBRARY_TYPE.AUDIO),
     },
     {
       icon: createElement(Icon, { path: mdiFileMultiple, size: 1.5 }),
       name: t("magneto.card.type.file"),
-      action: () => handleClickMedia("attachment"),
+      action: () => handleClickMedia(MEDIA_LIBRARY_TYPE.ATTACHMENT),
     },
     {
       icon: createElement(Icon, { path: mdiLink, size: 1.5 }),
       name: t("magneto.card.type.link"),
-      action: () => handleClickMedia("hyperlink"),
+      action: () => handleClickMedia(MEDIA_LIBRARY_TYPE.HYPERLINK),
     },
     {
       icon: createElement(Icon, {

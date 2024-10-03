3fc25e72defdd03faf109257cd768280116fe925
Florent Mariotti <florentcorse@gmail.com>
2024-10-03 14:32:51 +0200

    feat(MediaLibrary) : add MediaLibrary and its usage within the SideMenu (#298)
    ---------
    
    Co-authored-by: FlorentMr <florent.mariotti@cgi.com>

---
+++ frontend/src/providers/MediaLibraryProvider/types.ts
@@ -0,0 +1,32 @@
+import { Dispatch, ReactNode, RefObject, SetStateAction } from "react";
+
+import {
+  MediaLibraryRef,
+  MediaLibraryResult,
+  MediaLibraryType,
+  TabsItemProps,
+} from "@edifice-ui/react";
+import { WorkspaceElement } from "edifice-ts-client";
+
+import { MediaProps } from "~/components/board-view/types";
+
+export interface MediaLibraryProviderProps {
+  children: ReactNode;
+}
+
+export type MediaLibraryContextType = {
+  mediaLibraryRef: RefObject<MediaLibraryRef>;
+  libraryMedia: any;
+  mediaLibraryHandlers: {
+    setLibraryMedia: Dispatch<any>;
+    onCancel: (uploads?: WorkspaceElement[]) => Promise<void>;
+    onSuccess: (result: MediaLibraryResult) => void;
+    onTabChange: (
+      _tab: TabsItemProps,
+      uploads?: WorkspaceElement[],
+    ) => Promise<void>;
+  };
+  media: MediaProps | null;
+  setMedia: Dispatch<SetStateAction<MediaProps | null>>;
+  handleClickMedia: (type: MediaLibraryType) => void;
+};

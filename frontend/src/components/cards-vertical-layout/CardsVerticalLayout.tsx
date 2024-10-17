40ab07707ce4185285cbb84ebdf10857c6f6543e
alicedraillard <85180705+alicedraillard@users.noreply.github.com>
2024-10-17 18:09:44 +0200

    feat(dnd) : #MAG-486 dnd impl vertical and horizontal (#305)
    * incoming
    
    * wip
    
    * on the way
    
    * wip
    
    * wip 2
    
    * wip 3
    
    * wip ça marche
    
    * wip : working anim near ok
    
    * wip 100% fonctionnel
    
    * feat(dnd) : #MAG-486 dnd impl vertical and horizontal
    
    * prettier
    
    * fix lint error
    
    * fix enum review
    
    * fix some style
    
    * nocards horizontal style fixed
    
    ---------
    
    Co-authored-by: Thomas Sevagen <thomas.sevagen@cgi.com>

--- frontend/src/components/cards-vertical-layout/CardsVerticalLayout.tsx
+++ frontend/src/components/cards-vertical-layout/CardsVerticalLayout.tsx
@@ -1,5 +1,7 @@
 import { FC } from "react";
 
+import { DndContext, DragOverlay } from "@dnd-kit/core";
+import { rectSortingStrategy, SortableContext } from "@dnd-kit/sortable";
 import { Box } from "@mui/material";
 
 import {
@@ -10,48 +12,126 @@ import {
   CardsWrapper,
 } from "./style";
 import { BoardCard } from "../board-card/BoardCard";
+import { DndSection } from "../dnd-section/DndSection";
 import { SectionName } from "../section-name/SectionName";
+import { useSectionsDnD } from "~/hooks/dnd-hooks/useSectionsDnD";
+import { Card } from "~/models/card.model";
 import { useBoard } from "~/providers/BoardProvider";
 
 export const CardsVerticalLayout: FC = () => {
   const { board, zoomLevel, hasEditRights } = useBoard();
+  const {
+    activeItem,
+    updatedSections,
+    sensors,
+    handleDragStart,
+    handleDragOver,
+    handleDragEnd,
+    handleDragCancel,
+  } = useSectionsDnD(board);
 
-  if (!board.sections?.length) return null;
+  if (!updatedSections.length) return null;
 
   return (
-    <Box sx={mainWrapperProps}>
-      {board.sections.map((section) => (
-        <SectionWrapper
-          key={section._id}
-          sectionNumber={
-            hasEditRights() ? board.sections.length + 1 : board.sections.length
-          }
-        >
-          <Box sx={sectionNameWrapperStyle}>
-            <SectionName section={section} />
-          </Box>
-          <CardsWrapper zoomLevel={zoomLevel}>
-            {section.cards.map((card) => (
-              <CardWrapper key={card.id}>
-                <BoardCard
-                  card={card}
-                  zoomLevel={zoomLevel}
-                  canComment={board.canComment}
-                  displayNbFavorites={board.displayNbFavorites}
-                  key={card.id}
-                />
-              </CardWrapper>
-            ))}
-          </CardsWrapper>
-        </SectionWrapper>
-      ))}
-      {hasEditRights() && (
-        <SectionWrapper sectionNumber={board.sections.length + 1} isLast={true}>
-          <Box sx={sectionNameWrapperStyle}>
-            <SectionName section={null} />
-          </Box>
-        </SectionWrapper>
-      )}
-    </Box>
+    <DndContext
+      sensors={sensors}
+      onDragStart={handleDragStart}
+      onDragOver={handleDragOver}
+      onDragEnd={handleDragEnd}
+      onDragCancel={handleDragCancel}
+    >
+      <SortableContext
+        items={updatedSections.map((section) => section._id)}
+        strategy={rectSortingStrategy}
+      >
+        <Box sx={mainWrapperProps}>
+          {updatedSections.map((section) => (
+            <DndSection
+              key={section._id}
+              id={section._id}
+              noCards={!section.cards.length}
+              sectionType="vertical"
+              sectionNumber={
+                hasEditRights()
+                  ? updatedSections.length + 1
+                  : updatedSections.length
+              }
+            >
+              <Box sx={sectionNameWrapperStyle}>
+                <SectionName section={section} />
+              </Box>
+              <CardsWrapper zoomLevel={zoomLevel}>
+                <SortableContext
+                  items={section.cardIds}
+                  strategy={rectSortingStrategy}
+                >
+                  {section.cards.map((card: Card) => (
+                    <CardWrapper key={card.id}>
+                      <BoardCard
+                        card={card}
+                        zoomLevel={zoomLevel}
+                        canComment={board.canComment}
+                        displayNbFavorites={board.displayNbFavorites}
+                      />
+                    </CardWrapper>
+                  ))}
+                </SortableContext>
+              </CardsWrapper>
+            </DndSection>
+          ))}
+          {hasEditRights() && (
+            <DndSection
+              sectionNumber={updatedSections.length + 1}
+              isLast={true}
+              noCards={true}
+              sectionType="vertical"
+              id="new-section"
+            >
+              <Box sx={sectionNameWrapperStyle}>
+                <SectionName section={null} />
+              </Box>
+            </DndSection>
+          )}
+        </Box>
+      </SortableContext>
+      <DragOverlay>
+        {activeItem &&
+          ("cards" in activeItem ? (
+            <SectionWrapper
+              key={activeItem._id}
+              sectionNumber={
+                hasEditRights()
+                  ? updatedSections.length + 1
+                  : updatedSections.length
+              }
+            >
+              <Box sx={sectionNameWrapperStyle}>
+                <SectionName section={activeItem} />
+              </Box>
+              <CardsWrapper zoomLevel={zoomLevel}>
+                {activeItem.cards.map((card: Card) => (
+                  <CardWrapper key={card.id}>
+                    <BoardCard
+                      card={card}
+                      zoomLevel={zoomLevel}
+                      canComment={board.canComment}
+                      displayNbFavorites={board.displayNbFavorites}
+                    />
+                  </CardWrapper>
+                ))}
+              </CardsWrapper>
+            </SectionWrapper>
+          ) : (
+            <CardWrapper>
+              <BoardCard
+                card={activeItem as Card}
+                zoomLevel={zoomLevel}
+                canComment={board.canComment}
+                displayNbFavorites={board.displayNbFavorites}
+              />
+            </CardWrapper>
+          ))}
+      </DragOverlay>
+    </DndContext>
   );
 };

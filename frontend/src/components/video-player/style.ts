36d22ff4798a06c502bf0dfbe62f5eb43209ec26
Florent Mariotti <florentcorse@gmail.com>
2024-10-18 15:35:18 +0200

    feat(card) : #MAG-397 add video card creation (#309)
    ---------
    
    Co-authored-by: FlorentMr <florent.mariotti@cgi.com>

---
+++ frontend/src/components/video-player/style.ts
@@ -0,0 +1,25 @@
+export const videoWrapperStyle = {
+  width: "100%",
+  height: "100%",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  position: "relative",
+};
+
+export const videoContainerStyle = {
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  width: "100%",
+  marginBottom: "1rem",
+};
+
+export const videoStyle = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  position: "relative",
+  overflow: "hidden",
+};

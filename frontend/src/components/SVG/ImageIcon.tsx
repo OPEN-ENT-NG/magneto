b3d000a7f5aec35e45c8904583c16250e57c37c1
FlorentMr <florent.mariotti@cgi.com>
2024-10-22 12:02:22 +0200

    fix(svg) : #MAG-399 fix svg having wrong color sometimes

--- frontend/src/components/SVG/ImageIcon.tsx
+++ frontend/src/components/SVG/ImageIcon.tsx
@@ -5,36 +5,68 @@ import { CustomSVGProps } from "./types";
 export const ImageIcon: FC<CustomSVGProps> = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 22.852 31.613"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
     width="100%"
     height="100%"
+    viewBox="0 0 22.851549 31.61331"
   >
     <defs>
       <linearGradient
-        id="gradient"
+        id="gradient-1"
         gradientUnits="userSpaceOnUse"
-        x1="28.598"
-        y1="20.609"
-        x2="33.695"
-        y2="16.368"
+        x1="28.597717"
+        y1="20.608883"
+        x2="33.694511"
+        y2="16.367947"
       >
-        <stop offset="0" stopColor="#d96000" />
-        <stop offset="1" stopColor="#ff933c" stopOpacity="0" />
+        <stop offset="0" style={{ stopColor: "#d96000", stopOpacity: 1 }} />
+        <stop offset="1" style={{ stopColor: "#ff933c", stopOpacity: 0 }} />
       </linearGradient>
     </defs>
-    <path
-      d="M2.08 0h14.486l6.285 6.333v23.2a2.08 2.08 0 01-2.08 2.08H2.08A2.08 2.08 0 010 29.533V2.08A2.08 2.08 0 012.08 0z"
-      fill="#ff933c"
-    />
-    <path
-      d="M22.851 6.333l-4.885.018c-.997.004-1.406-.707-1.406-1.663l.005-4.688z"
-      fill="url(#gradient)"
-      opacity=".762"
-    />
-    <path
-      d="M8.724 16.033l1.701 2.041 2.381-3.062 3.062 4.082h-9.525m10.885.68v-9.524a1.361 1.361 0 00-1.36-1.361H7.363a1.361 1.361 0 00-1.36 1.36v9.525a1.361 1.361 0 001.36 1.36h9.525a1.361 1.361 0 001.36-1.36z"
-      fill="#fff"
-    />
-    <circle cx="8.043" cy="12.018" r="1.247" fill="#ff933c" />
+
+    <g transform="translate(-334.67959,-49.712309)">
+      <g transform="translate(382.97852,35.720502)">
+        <g transform="translate(-59.868369,-0.80250529)">
+          <path
+            style={{
+              fill: "#ff933c",
+              fillOpacity: 1,
+              stroke: "none",
+              strokeWidth: 4.26188,
+              strokeLinecap: "round",
+              strokeLinejoin: "round",
+            }}
+            d="m 13.649965,14.794317 h 14.485507 l 6.285512,6.332759 v 23.200019 c 0,1.152612 -0.927915,2.080527 -2.080527,2.080527 H 13.649965 c -1.152612,0 -2.080527,-0.927915 -2.080527,-2.080527 V 16.874844 c 0,-1.152612 0.927915,-2.080527 2.080527,-2.080527 z"
+          />
+          <path
+            style={{
+              opacity: 0.762238,
+              fill: "url(#gradient-1)",
+              stroke: "none",
+              strokeWidth: 2.235,
+              strokeLinecap: "round",
+              strokeLinejoin: "round",
+            }}
+            d="m 34.420987,21.127077 -4.885447,0.01779 c -0.996393,0.0036 -1.405545,-0.706498 -1.405545,-1.662989 l 0.0054,-4.687566 z"
+          />
+        </g>
+        <path
+          style={{ fill: "#ffffff", strokeWidth: 0.680335 }}
+          d="m -39.326168,30.753541 1.700842,2.041007 2.381175,-3.061512 3.061511,4.082016 h -9.524701 m 10.885374,0.680335 v -9.524701 c 0,-0.755175 -0.612303,-1.360673 -1.360673,-1.360673 h -9.524701 a 1.3606719,1.3606719 0 0 0 -1.360673,1.360673 v 9.524701 a 1.3606719,1.3606719 0 0 0 1.360673,1.360673 h 9.524701 a 1.3606719,1.3606719 0 0 0 1.360673,-1.360673 z"
+        />
+        <circle
+          style={{
+            fill: "#ff933c",
+            fillOpacity: 1,
+            stroke: "none",
+            strokeWidth: 0.944324,
+            strokeLinejoin: "round",
+          }}
+          cx="-40.00713"
+          cy="26.737717"
+          r="1.2466022"
+        />
+      </g>
+    </g>
   </svg>
 );
--- frontend/src/components/SVG/TextIcon.tsx
+++ frontend/src/components/SVG/TextIcon.tsx
@@ -5,36 +5,94 @@ import { CustomSVGProps } from "./types";
 export const TextIcon: FC<CustomSVGProps> = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 22.852 31.613"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
     width="100%"
     height="100%"
+    viewBox="0 0 22.851549 31.61331"
   >
     <defs>
       <linearGradient
-        id="gradient"
+        id="gradient-text"
         gradientUnits="userSpaceOnUse"
-        x1="28.598"
-        y1="20.609"
-        x2="32.183"
-        y2="17.502"
+        x1="28.597717"
+        y1="20.608883"
+        x2="32.182606"
+        y2="17.501875"
       >
-        <stop offset="0" stopColor="#114bbb" />
-        <stop offset="1" stopColor="#114bbb" stopOpacity="0" />
+        <stop style={{ stopColor: "#114bbb", stopOpacity: 1 }} offset="0" />
+        <stop style={{ stopColor: "#114bbb", stopOpacity: 0 }} offset="1" />
       </linearGradient>
     </defs>
-    <path
-      d="M2.08 0h14.486l6.285 6.333v23.2a2.08 2.08 0 01-2.08 2.08H2.08A2.08 2.08 0 010 29.533V2.08A2.08 2.08 0 012.08 0z"
-      fill="#5388ef"
-    />
-    <path
-      d="M22.851 6.333l-4.885.018c-.997.004-1.406-.707-1.406-1.663l.005-4.688z"
-      fill="url(#gradient)"
-      opacity=".762"
-    />
-    <g fill="#fff">
-      <rect x="3.911" y="9.521" width="14.815" height="2.109" ry="1.055" />
-      <rect x="3.999" y="14.948" width="14.815" height="2.109" ry="1.055" />
-      <rect x="4.085" y="20.144" width="14.815" height="2.109" ry="1.055" />
+
+    <g transform="translate(-139.00875,-92.294535)">
+      <g transform="translate(127.43931,77.500223)">
+        <path
+          style={{
+            fill: "#5388ef",
+            fillOpacity: 1,
+            stroke: "none",
+            strokeWidth: 4.26188,
+            strokeLinecap: "round",
+            strokeLinejoin: "round",
+          }}
+          d="m 13.649965,14.794317 h 14.485507 l 6.285512,6.332759 v 23.200019 c 0,1.152612 -0.927915,2.080527 -2.080527,2.080527 H 13.649965 c -1.152612,0 -2.080527,-0.927915 -2.080527,-2.080527 V 16.874844 c 0,-1.152612 0.927915,-2.080527 2.080527,-2.080527 z"
+        />
+        <g transform="translate(0,-1.6036173)">
+          <rect
+            style={{
+              fill: "#ffffff",
+              stroke: "none",
+              strokeWidth: 0.886382,
+              strokeLinecap: "round",
+              strokeLinejoin: "round",
+            }}
+            width="14.815158"
+            height="2.1092067"
+            x="15.481396"
+            y="25.816023"
+            ry="1.0546033"
+          />
+          <rect
+            style={{
+              fill: "#ffffff",
+              stroke: "none",
+              strokeWidth: 0.88638,
+              strokeLinecap: "round",
+              strokeLinejoin: "round",
+            }}
+            width="14.815158"
+            height="2.1092067"
+            x="15.569195"
+            y="31.242617"
+            ry="1.0546033"
+          />
+          <rect
+            style={{
+              fill: "#ffffff",
+              stroke: "none",
+              strokeWidth: 0.88638,
+              strokeLinecap: "round",
+              strokeLinejoin: "round",
+            }}
+            width="14.815158"
+            height="2.1092067"
+            x="15.654999"
+            y="36.438961"
+            ry="1.0546033"
+          />
+        </g>
+        <path
+          style={{
+            opacity: 0.762238,
+            fill: "url(#gradient-text)",
+            stroke: "none",
+            strokeWidth: 2.235,
+            strokeLinecap: "round",
+            strokeLinejoin: "round",
+          }}
+          d="m 34.420987,21.127077 -4.885447,0.01779 c -0.996393,0.0036 -1.405545,-0.706498 -1.405545,-1.662989 l 0.0054,-4.687566 z"
+        />
+      </g>
     </g>
   </svg>
 );

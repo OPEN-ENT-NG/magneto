e11619761adca296439ac316b82ee1ad7bf53ec3
Thomas Sevagen <thomas.sevagen@cgi.com>
2024-11-19 18:14:36 +0100

    feat(CardContent) : #MAG-412 add video previews (#334)
    * wip
    
    * feat(CardContent) : #MAG-412 add video previews

--- frontend/src/components/card-content-svg-display/types.ts
+++ frontend/src/components/card-content-svg-display/types.ts
@@ -1,4 +1,9 @@
 export interface CardContentSvgDisplayProps {
   extension: string;
   url?: string;
+  isPreview?: boolean;
+}
+
+export interface IsPreviewProps {
+  isPreview: boolean;
 }
--- frontend/src/components/preview-content/utils.tsx
+++ frontend/src/components/preview-content/utils.tsx
@@ -1,4 +1,11 @@
+import { Box } from "@mui/material";
+
+import { redirect } from "../card-content/utils";
 import { CardContentAudio } from "../card-content-audio/CardContentAudio";
+import { CardContentImageDisplay } from "../card-content-image-display/CardContentImageDisplay";
+import { CardContentSvgDisplay } from "../card-content-svg-display/CardContentSvgDisplay";
+import { ExternalVideoPlayer } from "../external-video-player/ExternalVideoPlayer";
+import { getVideoSource } from "../external-video-player/utils";
 import { PreviewContentImage } from "../preview-content-image/PreviewContentImage";
 import { RESOURCE_TYPE } from "~/core/enums/resource-type.enum";
 import { Card } from "~/models/card.model";
@@ -6,10 +13,27 @@ import { Card } from "~/models/card.model";
 export const displayPreviewContentByType = (card: Card) => {
   const cardType = card.resourceType as RESOURCE_TYPE;
   switch (cardType) {
-    case RESOURCE_TYPE.VIDEO:
-      return null;
+    case RESOURCE_TYPE.VIDEO: {
+      const videoSource = getVideoSource(card.resourceUrl);
+      return videoSource ? (
+        <ExternalVideoPlayer url={card.resourceUrl} source={videoSource} />
+      ) : (
+        <CardContentImageDisplay url={card.resourceUrl} />
+      );
+    }
     case RESOURCE_TYPE.LINK:
-      return null;
+      return (
+        <Box
+          onClick={() => redirect(card.resourceUrl)}
+          sx={{ cursor: "pointer" }}
+        >
+          <CardContentSvgDisplay
+            url={card.resourceUrl}
+            extension="link"
+            isPreview
+          />
+        </Box>
+      );
     case RESOURCE_TYPE.TEXT:
       return null;
     case RESOURCE_TYPE.IMAGE:

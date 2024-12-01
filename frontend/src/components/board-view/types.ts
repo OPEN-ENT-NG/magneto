db83d50ea6f6e7cb78cf6579e3f1df7aa10aeeed
Thomas Sevagen <thomas.sevagen@cgi.com>
2024-12-01 02:21:00 +0100

    fixed(dropZone): fixed textpopup style (#349)

--- frontend/src/components/board-view/types.ts
+++ frontend/src/components/board-view/types.ts
@@ -1,5 +1,7 @@
 import { MediaLibraryType } from "@edifice-ui/react";
 
+import { LAYOUT_TYPE } from "~/core/enums/layout-type.enum";
+
 export interface MediaProps {
   id: string;
   name: string;
@@ -11,4 +13,13 @@ export interface MediaProps {
 
 export interface IsModalOpenState {
   magnet: boolean;
+}
+
+export interface BoardBodyWrapperProps {
+  layout: LAYOUT_TYPE;
+  headerHeight: number;
+}
+
+export interface BoardViewWrapperProps {
+  layout: LAYOUT_TYPE;
 }

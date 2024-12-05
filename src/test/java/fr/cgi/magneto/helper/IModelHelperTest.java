da5c5dd7c50c52710a4d2b7846cb4d6b3e872580
alicedraillard <alice.draillard@cgi.com>
2024-12-05 17:33:19 +0100

    fix: fix unit tests following upgrade

--- src/test/java/fr/cgi/magneto/helper/IModelHelperTest.java
+++ src/test/java/fr/cgi/magneto/helper/IModelHelperTest.java
@@ -104,25 +104,4 @@ public class IModelHelperTest {
 
         ctx.assertTrue(invalidModel.isEmpty(), "One or more IModel don't have public constructor with JsonObject parameter declared. Check log above.");
     }
-
-    @Test
-    public void testLinkedMap(TestContext ctx) {
-        LinkedHashMap<Object, Object> linkedHashMap = new LinkedHashMap<>();
-        LinkedHashMap<Object, Object> linkedHashMap2 = new LinkedHashMap<>();
-        LinkedHashMap<Object, Object> linkedHashMap3 = new LinkedHashMap<>();
-        LinkedHashMap<Object, Object> linkedHashMap4 = new LinkedHashMap<>();
-        linkedHashMap.put("my_name", "name1");
-        linkedHashMap2.put("my_name", "name2");
-        //Is instance is not a MyOtherIModel, so it will not be added to the list
-        linkedHashMap3.put("my_name", new MyClass());
-        linkedHashMap4.put(3, "name3");
-
-        List<MyOtherIModel> list = ModelHelper.toList(new JsonArray(Arrays.asList(linkedHashMap, linkedHashMap2, linkedHashMap3,
-                linkedHashMap4)), MyOtherIModel.class);
-
-        ctx.assertEquals(list.size(), 3);
-        ctx.assertEquals(list.get(0).myName, "name1");
-        ctx.assertEquals(list.get(1).myName, "name2");
-        ctx.assertEquals(list.get(2).myName, null);
-    }
 }

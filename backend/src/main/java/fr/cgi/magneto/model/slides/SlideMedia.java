523c988208fcf7e3206c61967955a389d3c394e4
Thomas Sevagen <thomas.sevagen@cgi.com>
2025-02-24 10:01:35 +0100

    feat(slide): update SlideMedia to include caption and version bump to 2.2-SNAPSHOT

--- backend/src/main/java/fr/cgi/magneto/model/slides/SlideMedia.java
+++ backend/src/main/java/fr/cgi/magneto/model/slides/SlideMedia.java
@@ -4,16 +4,16 @@ import org.apache.poi.xslf.usermodel.XMLSlideShow;
 import org.apache.poi.xslf.usermodel.XSLFSlide;
 import fr.cgi.magneto.helper.SlideHelper;
 
-
 public class SlideMedia extends Slide {
 
     private byte[] resourceData;
     private final String fileExtension;
+    private final String caption;
 
-    public SlideMedia(String title, String description, byte[] resourceData,
+    public SlideMedia(String title, String caption, byte[] resourceData,
             String fileExtension) {
         this.title = title;
-        this.description = description;
+        this.caption = caption;
         this.resourceData = resourceData;
         this.fileExtension = fileExtension;
     }
@@ -26,7 +26,7 @@ public class SlideMedia extends Slide {
 
         SlideHelper.createTitle(slide, title);
         SlideHelper.createImage(slide, resourceData, fileExtension);
-        SlideHelper.createLegend(slide, description);
+        SlideHelper.createLegend(slide, caption);
 
         return slide;
     }

4492067c6af29c82b2538e87431da06235fddae6
Florent Mariotti <florent.mariotti@cgi.com>
2025-03-05 18:04:07 +0100

    fix download not starting in navigator?

--- frontend/src/components/export-modal/ExportModal.tsx
+++ frontend/src/components/export-modal/ExportModal.tsx
@@ -7,10 +7,11 @@ import {
   Tab,
   Tabs,
   Typography,
-  Modal,
-  IconButton,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
 } from "@cgi-learning-hub/ui";
-import CloseIcon from "@mui/icons-material/Close";
 import DownloadIcon from "@mui/icons-material/Download";
 import { Trans, useTranslation } from "react-i18next";
 
@@ -27,13 +28,7 @@ import {
   tabStyle,
 } from "./style";
 import { ExportModalProps } from "./types";
-import {
-  closeButtonStyle,
-  headerStyle,
-  modalContainerStyle,
-  modalFooterStyle,
-  titleStyle,
-} from "../message-modal/style";
+import { titleStyle } from "../message-modal/style";
 import { useBoardsNavigation } from "~/providers/BoardsNavigationProvider";
 import { useExportBoardQuery } from "~/services/api/export.service.ts";
 
@@ -55,25 +50,38 @@ export const ExportModal: React.FunctionComponent<ExportModalProps> = ({
   };
 
   useEffect(() => {
-    if (data) {
-      const blob = new Blob([data], {
-        type: "application/zip",
-      });
+    const triggerDownload = async () => {
+      if (data) {
+        try {
+          // Créer un Blob à partir des données
+          const blob = new Blob([data], { type: "application/zip" });
 
-      const url = window.URL.createObjectURL(blob);
-      const link = document.createElement("a");
-      link.href = url;
-      link.setAttribute("download", `${selectedBoards[0]._title}.zip`);
-      document.body.appendChild(link);
-      link.click();
+          // Créer un objet URL pour le blob
+          const url = window.URL.createObjectURL(blob);
 
-      link.parentNode?.removeChild(link);
-      window.URL.revokeObjectURL(url);
+          // Créer un élément <a> pour le téléchargement
+          const link = document.createElement("a");
+          link.href = url;
+          link.download = `${selectedBoards[0]._title}.zip`;
 
-      setShouldFetch(false);
-      onClose();
-    }
-  }, [data]);
+          // Ajouter à la page et déclencher le téléchargement
+          document.body.appendChild(link);
+          link.click();
+
+          // Nettoyer
+          document.body.removeChild(link);
+          window.URL.revokeObjectURL(url);
+        } catch (downloadError) {
+          console.error("Erreur lors du téléchargement:", downloadError);
+        } finally {
+          setShouldFetch(false);
+          onClose();
+        }
+      }
+    };
+
+    triggerDownload();
+  }, [data, selectedBoards, onClose]);
 
   // Gestion des erreurs
   useEffect(() => {
@@ -84,112 +92,100 @@ export const ExportModal: React.FunctionComponent<ExportModalProps> = ({
   }, [error]);
 
   return (
-    <Modal
+    <Dialog
+      sx={{
+        "& .MuiDialog-paper": {
+          width: "40%",
+          maxWidth: "40%",
+        },
+      }}
       open={isOpen}
       onClose={onClose}
       aria-labelledby="modal-title"
       aria-describedby="modal-message"
     >
-      <Box sx={modalContainerStyle}>
-        <Box sx={headerStyle}>
-          <Typography
-            id="modal-title"
-            variant="h4"
-            component="h2"
-            sx={titleStyle}
-          >
-            {t("magneto.board.export")}
-          </Typography>
-          <IconButton
-            onClick={onClose}
-            aria-label="close"
-            sx={closeButtonStyle}
-          >
-            <CloseIcon fontSize="inherit" />
-          </IconButton>
-        </Box>
-
+      <DialogTitle fontWeight="bold" component="h2" sx={titleStyle}>
+        {t("magneto.board.export")}
+      </DialogTitle>
+      <DialogContent>
+        <Tabs
+          sx={tabsStyle}
+          value={tabValue}
+          onChange={() => console.log("to be")}
+          variant="scrollable"
+          scrollButtons="false"
+        >
+          <Tab
+            label={t("magneto.board.download")}
+            icon={<DownloadIcon fontSize="large" />}
+            sx={tabStyle}
+          />
+        </Tabs>
         <Box>
-          <Tabs
-            sx={tabsStyle}
-            value={tabValue}
-            onChange={() => console.log("to be")}
-            variant="scrollable"
-            scrollButtons="false"
-          >
-            <Tab
-              label={t("magneto.board.download")}
-              icon={<DownloadIcon fontSize="large" />}
-              sx={tabStyle}
-            />
-          </Tabs>
-          <Box>
-            {tabValue === 0 && (
-              <Box>
-                <Typography variant="h2" sx={exportTitleStyle}>
-                  {t("magneto.export.modal.format")}
-                </Typography>
-                <Typography sx={exportContentStyle}>
-                  {t("magneto.export.modal.content")}
-                </Typography>
-                <Alert severity="info">
-                  <Box sx={alertTitleStyle}>
-                    {t("magneto.export.modal.informations")}
-                  </Box>
-                  <Box component="ul" sx={alertListStyle}>
-                    <Box component="li" sx={alertListItemStyle}>
-                      <Box sx={alertListItemBulletStyle}>•</Box>
-                      <Box sx={alertListItemContentStyle}>
-                        <Trans
-                          ns="magneto"
-                          i18nKey="magneto.export.modal.text.1"
-                          components={{
-                            bold: <strong />,
-                          }}
-                        />
-                      </Box>
-                    </Box>
-                    <Box component="li" sx={alertListItemStyle}>
-                      <Box sx={alertListItemBulletStyle}>•</Box>
-                      <Box sx={alertListItemContentStyle}>
-                        <Trans
-                          ns="magneto"
-                          i18nKey="magneto.export.modal.text.2"
-                          components={{
-                            bold: <strong />,
-                          }}
-                        />
-                      </Box>
+          {tabValue === 0 && (
+            <Box>
+              <Typography variant="h3" sx={exportTitleStyle}>
+                {t("magneto.export.modal.format")}
+              </Typography>
+              <Typography sx={exportContentStyle}>
+                {t("magneto.export.modal.content")}
+              </Typography>
+              <Alert severity="info">
+                <Box sx={alertTitleStyle}>
+                  {t("magneto.export.modal.informations")}
+                </Box>
+                <Box component="ul" sx={alertListStyle}>
+                  <Box component="li" sx={alertListItemStyle}>
+                    <Box sx={alertListItemBulletStyle}>•</Box>
+                    <Box sx={alertListItemContentStyle}>
+                      <Trans
+                        ns="magneto"
+                        i18nKey="magneto.export.modal.text.1"
+                        components={{
+                          bold: <strong />,
+                        }}
+                      />
                     </Box>
                   </Box>
-                </Alert>
-              </Box>
-            )}
-          </Box>
+                  <Box component="li" sx={alertListItemStyle}>
+                    <Box sx={alertListItemBulletStyle}>•</Box>
+                    <Box sx={alertListItemContentStyle}>
+                      <Trans
+                        ns="magneto"
+                        i18nKey="magneto.export.modal.text.2"
+                        components={{
+                          bold: <strong />,
+                        }}
+                      />
+                    </Box>
+                  </Box>
+                </Box>
+              </Alert>
+            </Box>
+          )}
         </Box>
-
-        <Box sx={modalFooterStyle}>
-          <Button
-            variant="text"
-            color="primary"
-            size="medium"
-            sx={buttonStyle}
-            onClick={onClose}
-          >
-            {t("magneto.cancel")}
-          </Button>
-          <Button
-            variant="contained"
-            color="primary"
-            size="medium"
-            sx={buttonStyle}
-            onClick={handleExport}
-            loading={isLoading}
-          >
-            {t("magneto.board.download")}
-          </Button>
-        </Box>
-      </Box>
-    </Modal>
+      </DialogContent>
+      <DialogActions>
+        <Button
+          variant="text"
+          color="primary"
+          size="medium"
+          sx={buttonStyle}
+          onClick={onClose}
+        >
+          {t("magneto.cancel")}
+        </Button>
+        <Button
+          variant="contained"
+          color="primary"
+          size="medium"
+          sx={buttonStyle}
+          onClick={handleExport}
+          loading={isLoading}
+        >
+          {t("magneto.board.download")}
+        </Button>
+      </DialogActions>
+    </Dialog>
   );
 };

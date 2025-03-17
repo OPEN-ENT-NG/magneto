461f8a28510a0a9afa25d23b455c7151bffc8b7c
Florent Mariotti <florent.mariotti@cgi.com>
2025-03-17 16:49:16 +0100

    add temporary header and style

---
+++ frontend/src/app/root/style.ts
@@ -0,0 +1,9 @@
+export const mainStyle = {
+  padding: "0 !important",
+  width: "100%",
+  margin: "auto",
+  maxWidth: "93%",
+  minHeight: "calc(100vh - 65px)",
+  flexDirection: "column !important",
+  display: "flex !important",
+};

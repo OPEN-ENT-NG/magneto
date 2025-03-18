8dd686401ddc3d33cb1cd2f678efe4f01be5db03
Florent Mariotti <florent.mariotti@cgi.com>
2025-03-18 13:46:12 +0100

    add error page and remove useless lightbox

---
+++ frontend/src/components/empty-state-public/style.ts
@@ -0,0 +1,21 @@
+export const boxEmptyState = {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  width: "100%",
+};
+
+export const titleEmptyState = {
+  textAlign: "center",
+  fontWeight: "bold",
+  font: "Roboto",
+  paddingBottom: "2rem",
+};
+
+export const contentEmptyState = {
+  textAlign: "justify",
+  font: "Roboto",
+  fontSize: "1.6rem",
+  lineHeight: "150%",
+  letterSpacing: "0.15px",
+};
--- frontend/src/routes/board-public/index.tsx
+++ frontend/src/routes/board-public/index.tsx
@@ -1,25 +1,56 @@
+import { Box } from "@mui/material";
 import { ThemeProvider } from "@mui/material/styles";
 import { useParams } from "react-router-dom";
 
 import { BoardView } from "~/components/board-view/BoardView";
+import {
+  loadingContainerStyle,
+  loadingTextStyle,
+} from "~/components/card-content-board/style";
+import { EmptyStatePublic } from "~/components/empty-state-public/EmptyStatePublic";
 import { BoardProvider } from "~/providers/BoardProvider";
 import { useGetIsExternalQuery } from "~/services/api/boards.service"; // Importation du hook RTK Query
 import theme from "~/themes/theme";
+import "./removeDisconnectLightbox.scss";
 
 export const App = () => {
   // Appel au hook RTK Query
   const { id = "" } = useParams();
 
-  const { data: isExternalQueryAllowed } = useGetIsExternalQuery(id);
+  const { data: isExternalQueryAllowed, isLoading } = useGetIsExternalQuery(id);
 
-  // Si le résultat est faux, affiche une page d'erreur
-  if (!isExternalQueryAllowed?.isExternal) {
+  if (isLoading) {
     return (
       <ThemeProvider theme={theme}>
-        <div className="error-page">
-          <h1>Erreur d'accès</h1>
-          <p>Vous n'avez pas l'autorisation d'accéder à cette ressource.</p>
-        </div>
+        {isLoading && (
+          <Box sx={loadingContainerStyle}>
+            <Box sx={loadingTextStyle}>Chargement...</Box>
+          </Box>
+        )}
+      </ThemeProvider>
+    );
+  }
+
+  // Si le résultat est faux, affiche une page d'erreur
+  if (true) {
+    return (
+      <ThemeProvider theme={theme}>
+        <Box
+          sx={{
+            width: "100%",
+            height: "100vh",
+            display: "flex",
+            justifyContent: "center",
+            paddingTop: "20vh",
+          }}
+        >
+          <Box sx={{ width: "30%" }}>
+            <EmptyStatePublic
+              title={"Aucun tableau trouvé"}
+              description="Le tableau Magnéto que vous cherchez semble introuvable. Il se peut que l'URL soit mal orthographiée, ou que le tableau n'existe plus ou ne soit plus partagé."
+            />
+          </Box>
+        </Box>
       </ThemeProvider>
     );
   }

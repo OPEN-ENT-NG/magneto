4b578462a5d8251b0abfd575318599e96cd628f3
Florent Mariotti <florent.mariotti@cgi.com>
2025-03-31 15:35:23 +0200

    fix main route missing theme infos (merge issues)

--- backend/src/main/java/fr/cgi/magneto/controller/MagnetoController.java
+++ backend/src/main/java/fr/cgi/magneto/controller/MagnetoController.java
@@ -1,16 +1,9 @@
 package fr.cgi.magneto.controller;
 
-import org.entcore.common.controller.ControllerHelper;
-import org.entcore.common.events.EventStore;
-import org.entcore.common.events.EventStoreFactory;
-import org.entcore.common.http.filter.ResourceFilter;
-import org.entcore.common.http.filter.SuperAdminFilter;
-
 import fr.cgi.magneto.Magneto;
 import fr.cgi.magneto.config.MagnetoConfig;
 import fr.cgi.magneto.core.constants.Field;
 import fr.cgi.magneto.core.constants.Rights;
-import static fr.cgi.magneto.core.enums.Events.ACCESS;
 import fr.cgi.magneto.security.ViewRight;
 import fr.cgi.magneto.service.ServiceFactory;
 import fr.wseduc.rs.ApiDoc;
@@ -19,6 +12,13 @@ import fr.wseduc.security.ActionType;
 import fr.wseduc.security.SecuredAction;
 import io.vertx.core.http.HttpServerRequest;
 import io.vertx.core.json.JsonObject;
+import org.entcore.common.controller.ControllerHelper;
+import org.entcore.common.events.EventStore;
+import org.entcore.common.events.EventStoreFactory;
+import org.entcore.common.http.filter.ResourceFilter;
+import org.entcore.common.http.filter.SuperAdminFilter;
+
+import static fr.cgi.magneto.core.enums.Events.ACCESS;
 
 public class MagnetoController extends ControllerHelper {
 
@@ -62,19 +62,21 @@ public class MagnetoController extends ControllerHelper {
         @SecuredAction(value = "", type = ActionType.RESOURCE)
         public void viewReact(HttpServerRequest request) {
                 String websocketEndpoint = Field.DEV.equals(this.magnetoConfig.mode())
-                                ? String.format(":%s%s", this.magnetoConfig.websocketConfig().port(),
-                                                this.magnetoConfig.websocketConfig().endpointProxy())
-                                : this.magnetoConfig.websocketConfig().endpointProxy();
+                        ? String.format(":%s%s", this.magnetoConfig.websocketConfig().port(),
+                        this.magnetoConfig.websocketConfig().endpointProxy())
+                        : this.magnetoConfig.websocketConfig().endpointProxy();
 
                 Integer updateFrequency = this.magnetoConfig.magnetoUpdateFrequency();
                 Boolean isStandalone = this.magnetoConfig.getMagnetoStandalone();
+                String themePlatform = this.magnetoConfig.getThemePlatform();
                 String host = this.magnetoConfig.host();
 
                 JsonObject param = new JsonObject()
-                                .put(Field.WEBSOCKETENDPOINT, websocketEndpoint)
-                                .put(Field.MAGNETO_UPDATE_FREQUENCY, updateFrequency)
-                                .put(Field.MAGNETO_STANDALONE, isStandalone)
-                                .put(Field.HOST, host);
+                        .put(Field.WEBSOCKETENDPOINT, websocketEndpoint)
+                        .put(Field.MAGNETO_UPDATE_FREQUENCY, updateFrequency)
+                        .put(Field.MAGNETO_STANDALONE, isStandalone)
+                        .put(Field.HOST, host)
+                        .put(Field.CAMEL_THEME_PLATFORM, themePlatform);
                 renderView(request, param, "index.html", null);
                 eventStore.createAndStoreEvent(ACCESS.name(), request);
         }
@@ -100,7 +102,8 @@ public class MagnetoController extends ControllerHelper {
                         .put(Field.MAGNETO_UPDATE_FREQUENCY, updateFrequency)
                         .put(Field.MAGNETO_STANDALONE, isStandalone)
                         .put(Field.HOST, host)
-                        .put(Field.CAMEL_THEME_PLATFORM, themePlatform);renderView(request, param, "index.html", null);
+                        .put(Field.CAMEL_THEME_PLATFORM, themePlatform);
+                renderView(request, param, "index.html", null);
                 eventStore.createAndStoreEvent(ACCESS.name(), request);
         }
 }

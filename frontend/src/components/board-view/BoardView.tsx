aa5bd852fd175a1d708b7263dcd07ffe0a39de6f
JinDouang <jinhda.douangphrachanh@cgi.com>
2025-04-18 00:47:37 +0200

    chore: add cursor component

--- frontend/src/components/board-view/BoardView.tsx
+++ frontend/src/components/board-view/BoardView.tsx
@@ -142,17 +142,17 @@ export const BoardView: FC = () => {
   }, []);
 
   // TODO with websocket
-  // const renderCursors = users => {
-  //   return Object
-  //     .keys(users)
-  //     .map(uuid => {
-  //       const user = users[uuid]
-  //       return <Cursor 
-  //         key={uuid} 
-  //         userId={uuid} 
-  //         point={[ user.state.x, user.state.y ]} />
-  //     })
-  // }
+  const renderCursors = users => {
+    return Object
+      .keys(users)
+      .map(uuid => {
+        const user = users[uuid]
+        return <Cursor 
+          key={uuid} 
+          userId={uuid} 
+          point={[ user.state.x, user.state.y ]} />
+      })
+  }
   
 
 

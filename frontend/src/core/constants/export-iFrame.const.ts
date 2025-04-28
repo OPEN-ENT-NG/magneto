a5e5cb63e431200b60b2b5c2751afa6bdbbdee9a
Florent Mariotti <florent.mariotti@cgi.com>
2025-04-28 14:41:15 +0200

    change iframe parameters

--- frontend/src/core/constants/export-iFrame.const.ts
+++ frontend/src/core/constants/export-iFrame.const.ts
@@ -7,11 +7,11 @@
 export const getIframeCode = (
   isExternalInput: boolean,
   boardId: string,
-) => `<div style="width:100%; overflow:hidden;">
+) => `<div style="width:100%; height:100%; overflow:hidden;">
     <iframe src="${window.location.origin}${
       !isExternalInput
         ? `/magneto#/board/${boardId}/view`
         : `/magneto/public#/pub/${boardId}`
     }" 
-    style="width:125%; height:125%; transform: scale(0.8); transform-origin: 0 0;" scrolling="no"></iframe>
+    style="width:100%; height:70vh; display: block; border: none; margin: 0 auto;" scrolling="no" allowfullscreen></iframe>
 </div>`;

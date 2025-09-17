20b5899873f4e0de8230d595d801a6dabb5c6266
Florent Mariotti <florent.mariotti@cgi.com>
2025-09-17 16:05:43 +0200

    isMultiCluster conf

--- backend/src/main/java/fr/cgi/magneto/config/MagnetoConfig.java
+++ backend/src/main/java/fr/cgi/magneto/config/MagnetoConfig.java
@@ -13,6 +13,7 @@ public class MagnetoConfig {
     private final Integer magnetoUpdateFrequency;
     private final Boolean magnetoStandalone;
     private final String themePlatform;
+    private final Boolean isMultiCluster;
 
     private final Integer DEFAULT_MAGNETO_UPDATE_FREQUENCY = 10 * 1000; //refresh every 10 seconds by default
 
@@ -25,6 +26,7 @@ public class MagnetoConfig {
         this.magnetoUpdateFrequency = Math.max(tempFrequency, DEFAULT_MAGNETO_UPDATE_FREQUENCY);
         this.magnetoStandalone = config.getBoolean(Field.MAGNETO_STANDALONE_CONFIG, false);
         this.themePlatform = config.getString(Field.KEBAB_THEME_PLATFORM, "default");
+        this.isMultiCluster = config.getBoolean(IS_MULTI_CLUSTER, false);
     }
 
     public String getThemePlatform() {
@@ -51,6 +53,10 @@ public class MagnetoConfig {
         return magnetoStandalone;
     }
 
+    public Boolean getIsMultiCluster() {
+        return this.isMultiCluster;
+    }
+
     public static class WebsocketConfig {
         private static final int DEFAULT_PORT = 9091;
         public static final String DEFAULT_ENDPOINTPROXY = "/magneto/eventbus";
@@ -60,14 +66,12 @@ public class MagnetoConfig {
         private final String endpointProxy;
         private final Integer maxUsers;
         private final Integer maxUsersPerBoard;
-        private final Boolean isMultiCluster;
 
         public WebsocketConfig(JsonObject websocketConfig) {
             this.port = websocketConfig.getInteger(WSPORT, DEFAULT_PORT);
             this.endpointProxy = websocketConfig.getString(ENDPOINT_PROXY_HYPHENS, DEFAULT_ENDPOINTPROXY);
             this.maxUsers = websocketConfig.getInteger(MAX_USERS, DEFAULT__MAX_USERS);
             this.maxUsersPerBoard = websocketConfig.getInteger(MAX_USERS_PER_BOARD, DEFAULT_MAX_USERS_PER_BOARD);
-            this.isMultiCluster = websocketConfig.getBoolean(IS_MULTI_CLUSTER, false);
         }
 
         public Integer getPort() {
@@ -82,9 +86,6 @@ public class MagnetoConfig {
         }
         public Integer getMaxUsersPerBoard() {
             return this.maxUsersPerBoard;
-        }
-        public Boolean getIsMultiCluster() {
-            return this.isMultiCluster;
         }
 
     }

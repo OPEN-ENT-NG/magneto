52ba3c5e287cb13a07f725b8dc175d1db9379a81
Florent Mariotti <florent.mariotti@cgi.com>
2025-09-22 17:02:21 +0200

    WIP

--- frontend/src/core/constants/export-iFrame.const.ts
+++ frontend/src/core/constants/export-iFrame.const.ts
@@ -11,7 +11,7 @@ export const getIframeCode = (
     <iframe src="${window.location.origin}${
       !isExternalInput
         ? `/magneto#/board/${boardId}/view`
-        : `/magneto/public/embed#/pub/${boardId}`
+        : `/magneto/public#/pub/${boardId}`
     }" 
     style="width:100%; height:70vh; display: block; border: none; margin: 0 auto;" scrolling="no" allowfullscreen></iframe>
 </div>`;

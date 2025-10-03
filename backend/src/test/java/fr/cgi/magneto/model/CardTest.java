e93bbe6409e52bface57b24d346d76787f89e3c4
Florent Mariotti <florent.mariotti@cgi.com>
2025-10-03 12:11:01 +0200

    WIP

--- backend/src/test/java/fr/cgi/magneto/model/CardTest.java
+++ backend/src/test/java/fr/cgi/magneto/model/CardTest.java
@@ -1,7 +1,6 @@
 package fr.cgi.magneto.model;
 
 import fr.cgi.magneto.model.cards.Card;
-import fr.cgi.magneto.model.comments.Comment;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.unit.TestContext;
@@ -9,36 +8,36 @@ import io.vertx.ext.unit.junit.VertxUnitRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.util.ArrayList;
-
 @RunWith(VertxUnitRunner.class)
 public class CardTest {
 
     JsonObject cardJsonObject = new JsonObject()
-            .put("_id", "id")
+            .put("id", "id")
             .put("title", "title")
-            .put("resourceId", "resourceId")
             .put("resourceType", "resourceType")
+            .put("resourceId", "resourceId")
             .put("resourceUrl", "resourceUrl")
             .put("description", "description")
-            .put("lastModifierName", "lastModifierName")
-            .put("lastModifierId", "lastModifierId")
-            .put("creationDate", "creationDate")
+            .put("caption", "caption")
+            .put("isLocked", false)
+            .put("locked", false)
             .put("modificationDate", "modificationDate")
-            .put("nbOfComments", 0)
             .put("creationDate", "creationDate")
+            .put("lastComment", new JsonObject())
+            .put("nbOfComments", 0)
+            .put("metadata", null)
+            .put("boardId", "boardId")
+            .put("parentId", null)
+            .put("nbOfFavorites", 0)
+            .put("isLiked", false)
+            .put("liked", false)
+            .put("favoriteList", new JsonArray())
+            .put("canBeIframed", false)
+            .put("comments", new JsonArray())
             .put("ownerId", "ownerId")
             .put("ownerName", "ownerName")
-            .put("boardId", "boardId")
-            .put("isLocked", false)
-            .put("lastComment", new JsonObject())
-            .putNull("metadata")
-            .putNull("parentId")
-            .put("caption", "caption")
-            .put("nbOfFavorites",0)
-            .put("isLiked",false)
-            .put("favoriteList", new JsonArray().getList())
-            .put("comments", new ArrayList<Comment>());
+            .put("lastModifierId", "lastModifierId")
+            .put("lastModifierName", "lastModifierName");
 
     @Test
     public void testCardHasBeenInstantiated(TestContext ctx) {
